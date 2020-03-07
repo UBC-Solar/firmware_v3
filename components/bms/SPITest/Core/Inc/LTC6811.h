5d4bed1bb9c0083e29476c2e1b3e9d700fe95bd3
a2k-hanlon <andrewkhanlon@gmail.com>
2020-03-07 14:58:38 -0800

    Fix type of SPI handle in LTC functions, fixed formatting

--- components/bms/SPITest/Core/Inc/LTC6811.h
+++ components/bms/SPITest/Core/Inc/LTC6811.h
@@ -1,5 +1,5 @@
 /*
- * LTC6811_SPI.h
+ * LTC6811.h
  *
  *  Created on: Feb. 14, 2020
  *      Author: Andrew Hanlon
@@ -27,48 +27,10 @@
 // LTC_pec15: returns the 2-byte CRC PEC generated from len bytes of data
 uint16_t LTC_pec15(uint8_t* data, int len);
 
-// LTC_wakeup: toggles the CS line to wake up the entire chain of LTC6811's
 void LTC_wakeup(void);
-
-// LTC_sendCmd: sends the 2-byte command followed by the 2-byte PEC over SPI
-void LTC_sendCmd(SPI_HandleTypeDef SPI_handle, uint16_t command);
-
-// LTC_writeRegisterGroup: writes the 6 bytes of a configuration register group in the LTC6811
-// Which register group is written to depends on the command provided (should be a WRxxxx command)
-void LTC_writeRegisterGroup(SPI_HandleTypeDef SPI_handle, uint16_t command, uint8_t TX_data[][6]);
-
-// LTC_readRegisterGroup: reads the 6 bytes of a configuration register group in the LTC6811
-// Which register group is read depends on the command provided (should be a RDxxxx command)
-int8_t LTC_readRegisterGroup(SPI_HandleTypeDef SPI_handle, uint16_t command, uint8_t RX_data[][6]);
-
-// LTC_readBatt: Poll ADCs and read registers for cell voltages
-// Returns -1 if there is a PEC mismatch, 0 otherwise
-// voltages is a 2 dimensional array of size NUM_LTC by 12 containing the cell voltages C1-C12 of
-// of each LTC6811 in the chain (nearest to MCU first)
-int8_t LTC_readBatt(SPI_HandleTypeDef SPI_handle, float voltages[][12]);
+void LTC_sendCmd(SPI_HandleTypeDef* SPI_handle, uint16_t command);
+void LTC_writeRegisterGroup(SPI_HandleTypeDef* SPI_handle, uint16_t command, uint8_t TX_data[][6]);
+int8_t LTC_readRegisterGroup(SPI_HandleTypeDef* SPI_handle, uint16_t command, uint8_t RX_data[][6]);
+int8_t LTC_readBatt(SPI_HandleTypeDef* SPI_handle, float voltages[][12]);
 
 #endif /* SRC_LTC6811_H_ */
-
-/*
- * The pec15Table lookup table was generated using the following code, modified from the example code
- * on pg. 76 of the LTC6811 Datasheet:
-
-#define CRC15_POLY 0x4599
-
-void init_PEC15_Table() {
-    unsigned short int remainder = 0;
-    for (int i = 0; i < 256; i++) {
-        remainder = i << 7;
-        for (int bit = 8; bit > 0; --bit) {
-            if (remainder & 0x4000) {
-                remainder = ((remainder << 1));
-                remainder = (remainder ^ CRC15_POLY);
-            }
-            else {
-                remainder = ((remainder << 1));
-            }
-        }
-        pec15Table[i] = remainder & 0xFFFF;
-    }
-}
- */
