30728e165e3b92d5bb8353786fe2e5cea7292722
Edward Ma <edwardma9234@gmail.com>
2020-03-21 14:29:39 -0700

    Filled out thermistor.c, added thermistor.h.

---
+++ components/bms/inc/thermistor.h
@@ -0,0 +1,12 @@
+//header file for thermistor.c
+#ifndef THERMISTOR_H
+#define THERMISTOR_H
+
+//TO MOVE TO 6811.h
+#define LTC_CMD_STCOMM      0b00000_111_0010_0011
+
+//Normal mode, gpio1
+#define LTC_CMD_ADAX_GPIO1  0b00000_01_10_1100_001
+#define LTC_CMD_RDAUXA      0b00000_000_0000_1100
+
+#endif // THERMISTOR_H
--- components/bms/src/thermistor.c
+++ components/bms/src/thermistor.c
@@ -1,1 +1,47 @@
-//hello world
+
+
+#include "LTC6811"
+//assumption: a single reading from each slave board's LTC6811
+
+void thermistor_mux(){
+  //start I2C (STCOMM)
+  LTC_sendCmd(LTC_CMD_STCOMM);
+
+  //a loop that reads for each of the 12 thermistor to the mux
+  // for(i = 1; i < 13; i++){
+  //   thermistor_reading(i)
+  // }
+}
+
+void thermistor_reading() {
+    //MAYBE RENAME TO "groupAvoltages"
+  uint8_t GPIO123voltages[NUM_LTC][6];
+  unit8_t RX_message[1] = {0};
+
+  //start conversion
+  //ADAX
+  LTC_sendCmd(LTC_CMD_ADAX_GPIO1);
+
+  //wait for Conversion to be ready.
+  do {
+    HAL_SPI_Receive(&RX_message, 1, 250);
+  } while (!RX_message)
+
+  //retrieve register
+  //RDAUXA
+  LTC_readRegisterGroup(SPI_handle, LTC_CMD_RDAUXA, GPIO123voltages[0]);
+
+
+  //volts2temp(GPIO123voltages[PLACEHOLDER]);
+  //there will probably be a better placement for the above func
+
+  //output reading
+}
+
+
+void volts2temp () {
+  const double beta = 3435.0;
+  const double max_ADC = 1023.0; //assuming ADC @ 10 bit
+  const double room_temp = 298.15;
+
+}
