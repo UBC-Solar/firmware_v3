c254791533954686365277456f42dda8f732dd59
a2k-hanlon <andrewkhanlon@gmail.com>
2020-04-05 12:50:54 -0700

    Andrew's edits

--- components/bms/inc/thermistor.h
+++ components/bms/inc/thermistor.h
@@ -1,32 +1,34 @@
-//header file for thermistor.c
-#ifndef THERMISTOR_H
-#define THERMISTOR_H
+#ifndef THERMISTOR_H_
+#define THERMISTOR_H_
 
-//TO MOVE TO 6811.h
-#define LTC_CMD_WRCOMM      0b00000_111_0010_0001
-#define LTC_CMD_RDCOMM      0b00000_111_0010_0010
-#define LTC_CMD_STCOMM      0b00000_111_0010_0011
-
-//Normal mode, gpio1
-#define LTC_CMD_ADAX_GPIO1  0b00000_01_10_1100_001
-#define LTC_CMD_RDAUXA      0b00000_000_0000_1100
+#include "stm32f3xx_hal.h"
+#include "ltc6811_btm.h"
 
 //for I2C communication
+#define MUX1_ADDRESS 0x90
+#define MUX2_ADDRESS 0x92
+// these include a 0 as LSB to signify I2C "write" operation
+
 //Table 19: Write Codes for ICOMn[3:0] and FCOMn[3:0] on I2C Master
 //ICOMn[3:0]
-#define I2C_START       0b0110
-#define I2C_STOP        0b0001
-#define I2C_BLANK       0b0000
-#define I2C_NO_TRANSMIT 0b1111
+typedef enum {
+	I2C_START = 0b0110,
+	I2C_STOP  = 0b0001,
+	I2C_BLANK = 0b0000,
+	I2C_NO_TRANSMIT = 0b1111
+} BTM_ICOM_t;
+
 //FCOMn[3:0]
-#define I2C_MASTER_ACK        0b0000
-#define I2C_MASTER_NACK       0b1000
-#define I2C_MASTER_NACK_STOP  0b1001
+typedef enum {
+	I2C_MASTER_ACK = 0b0000,
+	I2C_MASTER_NACK = 0b1000,
+	I2C_MASTER_NACK_STOP = 0b1001
+} BTM_FCOM_t;
 //MUX Control Codes for I2C control of multiplexer
 //Table 2. Multiplexer Control Bits Truth Table
 //sidenote: could drop 4th bit (first from left) of MUX_Sn,
 //then concatanate with MUX_ENABLE set to 1 bit 0b0
-#define MUX_ENABLE  0b0000
+#define MUX_DISABLE 0b0000
 #define MUX_S0      0b1000
 #define MUX_S1      0b1001
 #define MUX_S2      0b1010
@@ -39,7 +41,14 @@
 #define MUX_CHANNELS 6
 #define NUMBER_OF_MUX 2
 #define ONE_THIRD_OF_COMM 2
-//function prototypes
-void thermistor_reading(uint8_t GPIO1voltages[2]);
 
-#endif // THERMISTOR_H
+// Function prototypes
+void thermistor_series(void);
+void thermistor_mux(
+	uint8_t mux_address,
+	uint16_t thermistor_volt_series[MUX_CHANNELS][BTM_NUM_DEVICES]
+);
+void thermistor_reading(uint16_t GPIO1voltages[BTM_NUM_DEVICES]);
+void volts2temp(int ADC_val);
+
+#endif /* THERMISTOR_H_ */
--- components/bms/src/thermistor.c
+++ components/bms/src/thermistor.c
@@ -1,206 +1,184 @@
+#include "thermistor.h"
+#include <math.h>
 
-
-#include "LTC6811"
 //assumption: a single reading from each slave board's LTC6811
-
-
+// ^ That's not what you're doing here... is this an old note?
 
 /*
 Function name: thermistor_series
 Purpose: Algorithm for reading thermistors across multiple mux (2 mux's)
 input:
-  No user defined input.
-  Internal functions pull readings from hardware registers
+No user defined input.
+Internal functions pull readings from hardware registers
 Output:
-  uint8_t thermistor_volt_series[2][6], 2 sets of 6 voltages
+uint8_t thermistor_volt_series[2][6], 2 sets of 6 voltages
 Internal functions:
-  thermistor_mux()
-  thermistor_reading()
+thermistor_mux()
+thermistor_reading()
 */
-void thermistor_series(){
-  uint_8 COMM1 = 0b0000_0000;
-  uint_8 thermistor_volt_series[MUX_CHANNELS][NUM_LTC][ONE_THIRD_OF_COMM] = {0};
-    /*
-    Algorithm:
-      0) Initialize
-      1) read mux1
-        1.1) set mux_address or COMM1 bits for mux1
-        1.2) call thermistor_mux()
-        1.3) call thermistor_reading() SIX TIMES
-      2) read mux2
-        1.1) set mux_address or COMM1 bits for mux2
-        1.2) call thermistor_mux()
-        1.3) call thermistor_reading() SIX TIMES
-      3) done
-    */
+void thermistor_series()
+{
+	uint16_t thermistor_volt_series[ONE_THIRD_OF_COMM][MUX_CHANNELS][BTM_NUM_DEVICES] = { 0 };
+	/*
+	Algorithm:
+	0) Initialize
+	1) read mux1
+	1.1) set mux_address or COMM1 bits for mux1
+	1.2) call thermistor_mux()
+	1.3) call thermistor_reading() SIX TIMES
+	2) read mux2
+	1.1) set mux_address or COMM1 bits for mux2
+	1.2) call thermistor_mux()
+	1.3) call thermistor_reading() SIX TIMES
+	3) done
+	*/
 
-  //each cycle, the COMM1 address bits increment.
-  //this for-loop should only go up to 4 cycles,
-  //because there are only 4 possible mux address
-  for(int n = 0; n < NUMBER_OF_MUX; n++, COMM1 = COMM1 + 0b0010_0000){
-    thermistor_mux(COMM1, thermistor_volt_series);
-    }
-  }
+	// TODO: disable inactive mux before using the active one
+	thermistor_mux(MUX1_ADDRESS, thermistor_volt_series[0]);
+	thermistor_mux(MUX2_ADDRESS, thermistor_volt_series[1]);
 }
-
 
 /*
 Function name: thermistor_mux
 Purpose:
-  switch between all 12 channels OF A SINGLE MUX
-  and gather the associated themistor reading
+switch between all 12 channels OF A SINGLE MUX
+and gather the associated themistor reading
 input:
-  User defined:
-    COMM1, the exact address bits
-  internal functions pull readings from hardware registers
+User defined:
+COMM1, the exact address bits
+internal functions pull readings from hardware registers
 output: uint8_t thermistor_volt_array[12][2], 12 sets of data of 2 bytes
 */
-void thermistor_mux(uint8_t COMM1, uint_8 thermistor_volt_series[MUX_CHANNELS][NUM_LTC][ONE_THIRD_OF_COMM]){
 
-//the mux_message first dimension is redundant, it's more like the "table" indicator
-//i.e. MUX_message[0] contains all pointers to all "rows" of all of table 49 in datasheet
-//the rows are actually uint8_t variables for sanity sake; 1 table of 6 rows of y sized variables
-uint8_t MUX_message[1][6] = {0};
-uint8_t MUX_S[8] = {MUX_S0,
-                    MUX_S1,
-                    MUX_S2,
-                    MUX_S3,
-                    MUX_S4,
-                    MUX_S5,
-                    MUX_S6,
-                    MUX_S7};
+/* Algorithm of function (Note: this function is for a single mux)
+steps:
+0) known reset state: send to mux the enable signal?????
+1) control MUX with COMM registers
+1.1) WRCOMM: write to COMM register:
+Address half:
+1.1.1) ICOMn[3:0] = I2C_START
+1.1.2) D0[7:4] = 0x9, (Upper 4 bits of mux I2C address)
+1.1.3) D0[3:0] = Lower 3 bits of mux I2C address and a 0 for write
+1.1.4) FCOMn[3:0] = I2C_NACK
+Note: you release the bus on the acknowledge bit, so the mux can acknowledge
+A NACK is a high bit, and an open drain output going high is the same as releasing the bus
+Command half:
+1.1.5) ICOMn[3:0] = I2C_BLANK
+1.1.6) D1[7:4] = 0x0
+1.1.7) D1[3:0] = MUX_Sn
+1.1.8) FCOMn[3:0] = I2C_MASTER_NACK_STOP
+1.2) STCOMM: send the COMM register content
+1.2.1) read acknowledge bit???????? skip
+1.2.2) wait for appropriate clock cycles to pass using HAL_SPI_Transmit(NULLxsomething, sweet_nothings, idk)
+2) read GPIO1 for thermistor reading FOR each mux channel
+3) repeat until n = MUX_CHANNELS
+*/
+void thermistor_mux(uint8_t mux_address,
+	uint16_t thermistor_volt_series[MUX_CHANNELS][BTM_NUM_DEVICES])
+{
+	uint8_t MUX_S[8] = { MUX_S0, MUX_S1, MUX_S2, MUX_S3, MUX_S4, MUX_S5, MUX_S6, MUX_S7 };
+	uint8_t NULL_message[6] = { 0 };
+	uint8_t comm_val[6] =
+	{
+		(I2C_START << 4) | (mux_address >> 4), // 1.1.1) - 1.1.2)
+		(mux_address << 4) | I2C_MASTER_NACK,
+		0, // ((I2C_BLANK << 4) | 0x00) = 0, First nibble of mux command is 0
+		I2C_MASTER_NACK_STOP, // mux command in upper nibble is set in loop
+		0, // There's no 3rd byte to transmit
+		0
+	};
+	uint8_t mux_message[BTM_NUM_DEVICES][6] = { 0 };
 
-uint8_t COMM0;
-//uint8_t COMM1;
-uint8_t COMM2;
-uint8_t COMM3;
-uint8_t COMM4 = 0b0000_0000;
-uint8_t COMM5 = 0b0000_0000;
+	//1.1) write to COMM register
+	for (int n = 0; n < MUX_CHANNELS; n++)
+	{
+		// Address half (same every loop, set above):
+		// 1.1.1) ICOMn[3:0] = I2C_START
+		// 1.1.2) D0[7:4] = 0x9, (Upper 4 bits of mux I2C address)
+		// 1.1.3) D0[3:0] = Lower 3 bits of mux I2C address and a 0 for write
+		// 1.1.4) FCOMn[3:0] = I2C_NACK
+		// Command half:
+		// 1.1.5) ICOMn[3:0] = I2C_BLANK (same every loop, set above)
+		// 1.1.6) D1[7:4] = 0x0 (same every loop, set above)
 
-uint8_t NULL_message = 0b0000_0000;
+		//mux channel switching
+		//1.1.7) D1[3:0] = MUX_Sn
+		//1.1.8) FCOMn[3:0] = I2C_MASTER_NACK_STOP
+		comm_val[3] = MUX_S[n] << 4; // bit shifted value of MUX_S[n] == 0bXXXX_0000
+		comm_val[3] |= I2C_MASTER_NACK_STOP;
 
-  /* Algorithm of function (Note: this function is for a single mux)
-  steps:
-    0) known reset state: send to mux the enable signal?????
+		// Make enough copies of this data to send to all the LTC6811's
+		for(int ic_num = 0; ic_num < BTM_NUM_DEVICES; ic_num++)
+		{
+			for(int byte_num = 0; byte_num < 6; byte_num++)
+			{
+				mux_message[ic_num][byte_num] = comm_val[byte_num];
+			}
+		}
+		//sends from STM32 to 6811 for 6811 to send to MUX the I2C command
+		BTM_writeRegisterGroup(CMD_WRCOMM, mux_message);
 
-    1) control MUX with COMM registers
-      1.1) WRCOMM: write to COMM register:
+		// 1.2) STCOMM: send the COMM register content
+		HAL_GPIO_WritePin(BTM_CS_GPIO_PORT, BTM_CS_GPIO_PIN, 0); // pull CS low
+		BTM_sendCmd(CMD_STCOMM);
+		// driving SPI clock needed for I2C, 3 clock cycles per bit sent
+		// can be done by sending null data
+		// BTM_SPI_handle is a global variable,
+		// 	and BTM_TIMEOUT_VAL is a symbolic constant
+		HAL_SPI_Transmit(BTM_SPI_handle, NULL_message, 6, BTM_TIMEOUT_VAL);
+		HAL_GPIO_WritePin(BTM_CS_GPIO_PORT, BTM_CS_GPIO_PIN, 1); // pull CS high
 
-        Address half:
-        1.1.1) ICOMn[3:0] = I2C_START
-        1.1.2) D0[7:4] = 0b0000, (it's MSB)
-        1.1.3) D0[3:0] = address stuff (it's MSB)
-        1.1.4) FCOMn[3:0] = I2C_ACK (note that we assume ACK instead of NACK because NACK would tell the slave that is the end of the transmission)
-
-        Command half:
-        1.1.5) ICOMn[3:0] = I2C_BLANK
-        1.1.6) D1[7:4] = 0b0000, (it's MSB)
-        1.1.7) D1[3:0] = MUX_Sn  (it's MSB)
-        1.1.8) FCOMn[3:0] = I2C_MASTER_NACK_STOP
-
-      1.2) STCOMM: send the COMM register content
-        1.2.1) read acknowledge bit???????? skip
-        1.2.2) wait for appropriate clock cycles to pass using HAL_SPI_Transmit(NULLxsomething, sweet_nothings, idk)
-    2) read GPIO1 for thermistor reading FOR each mux channel
-    3) repeat until n = MUX_CHANNELS
-  */
-
-  //1.1) write to COMM register
-  for(int n = 0; n < MUX_CHANNELS; n++) {
-    //Address half of COMM message:
-    //1.1.1) ICOMn[3:0] = I2C_START
-    //1.1.2) D0[7:4] = 0b0000, (it's MSB)
-
-    //__ICOMM[3:0]_D[7:4]
-    COMM0 = 0b0110_1001; // 1001 is a defined number from LTC1380 datasheet p.8
-
-    //1.1.3) D0[3:0] = address stuff (it's MSB)
-    //1.1.4) FCOMn[3:0] = I2C_ACK (note that we assume ACK instead of NACK because NACK would tell the slave that is the end of the transmission)
-    //COMM1 assigned by the thermistor_series
-
-    MUX_message[0][0] = COMM0;
-    MUX_message[0][1] = COMM1;
-
-    //1.1.1) ICOMn[3:0] = I2C_BLANK
-    //1.1.6) D1[7:4] = 0b0000, (it's MSB)
-    COMM2 = 0b0000_0000;
-
-    //mux channel switching
-      //1.1.7) D1[3:0] = MUX_Sn  (it's MSB)
-      //1.1.8) FCOMn[3:0] = I2C_MASTER_NACK_STOP
-    COMM3 = MUX_S[n] << 4 //bit shifted value of MUX_S[n] == 0bXXXX_0000
-    COMM3 = COMM3 | I2C_MASTER_NACK_STOP;
-    MUX_message[0][2] = COMM2;
-    MUX_message[0][3] = COMM3;
-
-    //redundancy zero-fill for rest of COMM registers
-    MUX_message[0][4] = COMM4;
-    MUX_message[0][5] = COMM5;
-
-    //sends from STM32 to 6811 for 6811 to send to MUX the I2C command
-    LTC_writeRegisterGroup(LTC_CMD_WRCOMM, MUX_message[0]);
-
-    //1.2) STCOMM: send the COMM register content
-    LTC_sendCmd(LTC_CMD_STCOMM);
-
-
-    //timing delays needed for I2C, 3 clock cycles per bit sent
-    //can be done by sending null values to an unspecified register
-    //or by reading a "safe" register
-    HAL_SPI_Transmit(BTM_SPI_handle, &NULL_message, 1, BTM_TIMEOUT_VAL); // BTM_SPI_handle is a global variable, and BTM_TIMEOUT_VAL is a symbolic constant
-
-    //gather thermistor readings
-    thermistor_reading(thermistor_volt_series[n][0]);
-  }
+		//gather thermistor readings
+		thermistor_reading(thermistor_volt_series[n]);
+	}
 }
 
 /*
 Function name: thermistor_reading
 Purpose: get voltage readings of thermistors and output into a register
 input: internal functions pull readings from hardware registers
-output: uint8_t GPIO1voltages[2], two bytes long because that's how long it is.
+output: uint16_t GPIO1voltages, two bytes long because that's how long it is.
 */
-void thermistor_reading(uint8_t GPIO1voltages[NUM_LTC][ONE_THIRD_OF_COMM]) {
-  uint8_t GPIO123voltages[NUM_LTC][6];
-  unit8_t RX_message[1] = {0};
+void thermistor_reading(uint16_t GPIO1voltages[BTM_NUM_DEVICES])
+{
+	uint8_t GPIO123voltages[BTM_NUM_DEVICES][6];
+	uint16_t voltage_reading = 0;
 
-  //start conversion
-  //ADAX, ADc AuXillary start-conversion command
-  LTC_sendCmd(LTC_CMD_ADAX_GPIO1);
+	//start conversion
+	//ADAX, ADc AuXillary start-conversion command
+	BTM_sendCmdAndPoll(CMD_ADAX_GPIO1);
 
-  //wait for Conversion to be ready.
-  do {
-      HAL_SPI_Receive(LTC_SPI_handle, &RX_message, 1, 250);
-  } while (!RX_message);
+	//retrieve register readings
+	BTM_readRegisterGroup(CMD_RDAUXA, GPIO123voltages);
 
-  //retrieve register readings
-  //RDAUXA
-  LTC_readRegisterGroup(LTC_CMD_RDAUXA, GPIO123voltages[0]);
-
-  //output reading by assigning to pointed array the first two bytes of GPIO123voltages
-  for(int board = 1; board <= NUM_LTC; board++) {
-    for(int i = 0; i < 2; i++) {
-      GPIO1voltages[board][i] = GPIO123voltages[board][i];
-    }
-  }
-
+	//output reading by assigning to pointed array the first two bytes of GPIO123voltages
+	for (int board = 0; board < BTM_NUM_DEVICES; board++)
+	{
+		// Combine 2 bytes of voltage reading
+		voltage_reading = ( ((uint16_t) GPIO123voltages[board][1]) << 8)
+			| GPIO123voltages[board][0];
+		// Store in given array
+		GPIO1voltages[board] = voltage_reading;
+	}
 }
 
+// Perhaps you should return something...
+void volts2temp(int ADC_val)
+{
+	const double beta = 3435.0;
+	const double max_ADC = 65535.0; //assuming ADC @ 16 bit
+	const double room_temp = 298.15;
+	const double R_balance = 10000.0; //from LTC6811 datasheet p.85. note: this doesn't account for tolerance. to be exact, measure the 10k resistor with a multimeter
+	const double R_room_temp = 10000.0; //resistance at room temperature (25C)
+	//const int sample_num = 10;
+	double R_therm = 0;
+	double temp_kelvin = 0;
+	double temp_celsius = 0;
+	//double adc_average = 0; // we will do multiple measurements and average them to eliminate errors
+	//int adc_samples[sample_num];
 
-void volts2temp (ADC_val) {
-  const double beta = 3435.0;
-  const double max_ADC = 65535.0; //assuming ADC @ 16 bit
-  const double room_temp = 298.15;
-  const double R_balance = 10000.0; //from LTC6811 datasheet p.85. note: this doesn't account for tolerance. to be exact, measure the 10k resistor with a multimeter
-  const double R_room_temp = 10000.0; //resistance at room temperature (25C)
-  //const int sample_num = 10;
-  double R_therm = 0;
-  double temp_kelvin = 0;
-  double temp_celsius = 0;
-  //double adc_average = 0; //we will do multiple measurements and average them to eliminate errors
-  //int adc_samples[sample_num];
-
-  R_therm = R_balance * ((max_ADC / ADC_val) - 1);
-  temp_kelvin = (beta * room_temp) / (beta + (room_temp * log(R_therm/R_room_temp)));
-  temp_celsius = temp_kelvin - 273.15;
+	R_therm = R_balance * ((max_ADC / ADC_val) - 1);
+	temp_kelvin = (beta * room_temp)
+		/ (beta + (room_temp * log(R_therm / R_room_temp)));
+	temp_celsius = temp_kelvin - 273.15;
 }
