2c459d9bec00bbf57d1a924a3e4ee7036ebf2d8a
a2k-hanlon <andrewkhanlon@gmail.com>
2020-05-02 23:53:12 -0700

    Implemented control signals to ECU, reconfigured HAL for added pins

--- components/bms/SPITest/Core/Inc/control.h
+++ components/bms/SPITest/Core/Inc/control.h
@@ -2,12 +2,11 @@
  *  @file contactors.h
  *  @brief Header file for contactor control
  *
- *  The 4 contactors are:
- *  NEG  - Negative terminal; the main contactor switching the battery
- *  PCH  - Pre-charge; in parallel with LLIM, for inrush current limiting
- *           at startup
- *  HLIM - High limit; between battery and solar array input
- *  LLIM - Low limit; between batteries and motor controller output
+ *  Steps to set up this functionality:
+ *  1.  Set the GPIO port and pin for all signals in this file
+ *  2.  After initializing the STM32's TIM peripheral in the application code,
+ *      set the CONT_TIM_handle pointer to the HAL TIM handle
+ *      eg. CONT_TIM_handle = &htim3;
  *
  *  @date 2020/04/29
  *  @author Andrew Hanlon (a2k-hanlon)
@@ -22,30 +21,43 @@
 /* ENUMERATIONS */
 
 typedef enum {
-    CONT_OPEN = 0,
-    CONT_CLOSED = 1
-} CONT_contactor_state_t;
+    CONT_INACTIVE = 0,
+    CONT_ACTIVE = 1
+} CONT_signal_state_t;
 
 /*============================================================================*/
 /* CONFIGURABLE PARAMETERS */
 
-#define CONT_NEG_PORT GPIOA
-#define CONT_NEG_PIN 0
+#define CONT_BAL_PORT GPIOB
+#define CONT_BAL_PIN GPIO_PIN_12
 
-#define CONT_PCH_PORT GPIOA
-#define CONT_PCH_PIN 0
+#define CONT_COM_PORT GPIOB
+#define CONT_COM_PIN GPIO_PIN_13
 
-#define CONT_HLIM_PORT GPIOA
-#define CONT_HLIM_PIN 0
+#define CONT_FLT_PORT GPIOB
+#define CONT_FLT_PIN GPIO_PIN_14
+
+#define CONT_HLIM_PORT GPIOB
+#define CONT_HLIM_PIN GPIO_PIN_15
 
 #define CONT_LLIM_PORT GPIOA
-#define CONT_LLIM_PIN 0
+#define CONT_LLIM_PIN GPIO_PIN_8
 
-// Contactor polarities - the pin state when contactors are open
-#define CONT_NEG_POLARITY 0
-#define CONT_PCH_POLARITY 0
+#define CONT_OT_PORT GPIOA
+#define CONT_OT_PIN GPIO_PIN_9
+
+// For all signals, this is the inactive pin state
+// For LLIM and HLIM, this is the pin state when contactors should be closed
+#define CONT_BAL_POLARITY 0
+#define CONT_COM_POLARITY 0
+#define CONT_FLT_POLARITY 0
 #define CONT_HLIM_POLARITY 0
 #define CONT_LLIM_POLARITY 0
+#define CONT_OT_POLARITY 0
+
+#define CONT_TIM_CHANNEL TIM_CHANNEL_2 // must match the peripheral config
+#define PWM_DIVISIONS 320 // This should be equal to the value of the
+// auto-reload register of the timer used for PWM generation + 1
 
 /*============================================================================*/
 /* PUBLIC CONSTANTS */
@@ -53,19 +65,50 @@ typedef enum {
 /*============================================================================*/
 /* PUBLIC VARAIBLES */
 
-// Do not write to these in external code!
-CONT_contactor_state_t CONT_NEG_state;
-CONT_contactor_state_t CONT_PCH_state;
-CONT_contactor_state_t CONT_HLIM_state;
-CONT_contactor_state_t CONT_LLIM_state;
+TIM_HandleTypeDef * CONT_timer_handle;
+
+// For code outside control.c, consider these READ-ONLY
+CONT_signal_state_t CONT_BAL_state;
+CONT_signal_state_t CONT_COM_state;
+CONT_signal_state_t CONT_FLT_state;
+CONT_signal_state_t CONT_HLIM_state;
+CONT_signal_state_t CONT_LLIM_state;
+CONT_signal_state_t CONT_OT_state;
+unsigned int CONT_FAN_PWM_percent;
 
 /*============================================================================*/
 /* FUNCTION PROTOTYPES */
 
 void CONT_init(void);
-void CONT_NEG_switch(CONT_contactor_state_t new_state);
-void CONT_PCH_switch(CONT_contactor_state_t new_state);
-void CONT_HLIM_switch(CONT_contactor_state_t new_state);
-void CONT_LLIM_switch(CONT_contactor_state_t new_state);
+
+void CONT_BAL_switch(CONT_signal_state_t new_state);
+void CONT_COM_switch(CONT_signal_state_t new_state);
+void CONT_FLT_switch(CONT_signal_state_t new_state);
+void CONT_HLIM_switch(CONT_signal_state_t new_state);
+void CONT_LLIM_switch(CONT_signal_state_t new_state);
+void CONT_OT_switch(CONT_signal_state_t new_state);
+void CONT_FAN_PWM_set(unsigned int pwm_val);
 
 #endif /* INC_CONTROL_H_ */
+
+/**
+ * Note the following for configuring a timer for PWM:
+ * - Use a general purpose timer, not a basic timer.
+ * - Slave Mode: Disable
+ * - Trigger Source: Disable
+ * - Clock Source: Internal Clock
+ * - Configure the applicable channel for PWM generation
+ * - Autoreload value: greater than 100.
+ * - Prescaler: set in conjunction with the autoreload value so that
+ *              the PWM frequency is 25kHz.
+ *              Prescaler = Clock_Freq / ( 25000 * (autreload + 1)) - 1
+ * eg. for 8 MHz clock, could do autorelaod = 320 and a prescaler of 0
+ * - Upcounting timer
+ * - No internal clock division
+ * - auto-reload preload is not needed
+ * - PWM mode 1
+ * - Configure the pulse to 0; it will be set by CONT_FAN_PWM_set()
+ * - output compare preload enabled
+ * - CH polarity can be used to change the polarity like the
+ *   POLARITY constants above
+ */
--- components/bms/SPITest/Core/Src/control.c
+++ components/bms/SPITest/Core/Src/control.c
@@ -1,13 +1,17 @@
 /**
- *  @file contactors.c
- *  @brief Functions for controlling the 4 contactors in the battery
+ *  @file controls.c
+ *  @brief Functions for control signals to the ECU
  *
- *  The 4 contactors are:
- *  NEG  - Negative terminal; the main contactor switching the battery
- *  PCH  - Pre-charge; in parallel with LLIM, for inrush current limiting
- *           at startup
- *  HLIM - High limit; between battery and solar array input
- *  LLIM - Low limit; between batteries and motor controller output
+ *  The control signals are:
+ *  BAL  - Balancing active indication
+ *  COM  - Communication fault (with slave boards) indication
+ *  FLT  - General Fault indication (active for any fault)
+ *  GPIO - General Purpose Input Output - not implemented here; future proofing
+ *  HLIM - High limit; highest voltage module is over-voltage
+ *  LLIM - Low limit; lowest voltage module is under-voltage
+ *  OT   - Over temperature indication
+ *
+ *  FAN_PWM - Pulse width modulated signal for cooling fan control
  *
  *  @date 2020/04/29
  *  @author Andrew Hanlon (a2k-hanlon)
@@ -17,37 +21,68 @@
 
 void CONT_init()
 {
-    CONT_NEG_state = CONT_OPEN;
-    CONT_PCH_state = CONT_OPEN;
-    CONT_HLIM_state = CONT_OPEN;
-    CONT_LLIM_state = CONT_OPEN;
+    CONT_BAL_state = CONT_INACTIVE;
+    CONT_COM_state = CONT_INACTIVE;
+    CONT_FLT_state = CONT_INACTIVE;
+    CONT_HLIM_state = CONT_INACTIVE;
+    CONT_LLIM_state = CONT_INACTIVE;
+    CONT_OT_state = CONT_INACTIVE;
 
-    HAL_GPIO_WritePin(CONT_NEG_PORT, CONT_NEG_PIN, CONT_OPEN ^ CONT_NEG_POLARITY);
-    HAL_GPIO_WritePin(CONT_PCH_PORT, CONT_PCH_PIN, CONT_OPEN ^ CONT_PCH_POLARITY);
-    HAL_GPIO_WritePin(CONT_HLIM_PORT, CONT_HLIM_PIN, CONT_OPEN ^ CONT_HLIM_POLARITY);
-    HAL_GPIO_WritePin(CONT_LLIM_PORT, CONT_LLIM_PIN, CONT_OPEN ^ CONT_LLIM_POLARITY);
+    CONT_BAL_switch(CONT_INACTIVE);
+    CONT_COM_switch(CONT_INACTIVE);
+    CONT_FLT_switch(CONT_INACTIVE);
+    CONT_HLIM_switch(CONT_INACTIVE);
+    CONT_LLIM_switch(CONT_INACTIVE);
+    CONT_OT_switch(CONT_INACTIVE);
+
+    CONT_FAN_PWM_set(0);
+    HAL_TIM_PWM_Start(CONT_timer_handle, CONT_TIM_CHANNEL);
 }
 
-void CONT_NEG_switch(CONT_contactor_state_t new_state)
+void CONT_BAL_switch(CONT_signal_state_t new_state)
 {
-    HAL_GPIO_WritePin(CONT_NEG_PORT, CONT_NEG_PIN, new_state ^ CONT_NEG_POLARITY);
-    CONT_NEG_state = new_state;
+    HAL_GPIO_WritePin(CONT_BAL_PORT, CONT_BAL_PIN, new_state ^ CONT_BAL_POLARITY);
+    CONT_BAL_state = new_state;
 }
 
-void CONT_PCH_switch(CONT_contactor_state_t new_state)
+void CONT_COM_switch(CONT_signal_state_t new_state)
 {
-    HAL_GPIO_WritePin(CONT_PCH_PORT, CONT_PCH_PIN, new_state ^ CONT_PCH_POLARITY);
-    CONT_PCH_state = new_state;
+    HAL_GPIO_WritePin(CONT_COM_PORT, CONT_COM_PIN, new_state ^ CONT_COM_POLARITY);
+    CONT_COM_state = new_state;
 }
 
-void CONT_HLIM_switch(CONT_contactor_state_t new_state)
+void CONT_FLT_switch(CONT_signal_state_t new_state)
+{
+    HAL_GPIO_WritePin(CONT_FLT_PORT, CONT_FLT_PIN, new_state ^ CONT_FLT_POLARITY);
+    CONT_FLT_state = new_state;
+}
+
+void CONT_HLIM_switch(CONT_signal_state_t new_state)
 {
     HAL_GPIO_WritePin(CONT_HLIM_PORT, CONT_HLIM_PIN, new_state ^ CONT_HLIM_POLARITY);
     CONT_HLIM_state = new_state;
 }
 
-void CONT_LLIM_switch(CONT_contactor_state_t new_state)
+void CONT_LLIM_switch(CONT_signal_state_t new_state)
 {
     HAL_GPIO_WritePin(CONT_LLIM_PORT, CONT_LLIM_PIN, new_state ^ CONT_LLIM_POLARITY);
     CONT_LLIM_state = new_state;
+}
+
+void CONT_OT_switch(CONT_signal_state_t new_state)
+{
+    HAL_GPIO_WritePin(CONT_OT_PORT, CONT_OT_PIN, new_state ^ CONT_OT_POLARITY);
+    CONT_OT_state = new_state;
+}
+
+/**
+ * @brief Sets the PWM duty cycle of the FAN_PWM output.
+ *
+ * @param pwm_val The duty cycle of the PWM. Must be in range [0,100].
+ */
+void CONT_FAN_PWM_set(unsigned int pwm_val)
+{
+    unsigned int CONT_FAN_PWM_percent = pwm_val;
+    pwm_val = (pwm_val * PWM_DIVISIONS) / 100;
+    __HAL_TIM_SET_COMPARE(CONT_timer_handle, CONT_TIM_CHANNEL, pwm_val);
 }
