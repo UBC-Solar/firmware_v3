900933e1b21cd67e43fd289b06564596719cca01
Edward Ma <edwardma9234@gmail.com>
2020-05-27 17:19:40 -0700

    More 623() code. Added VoltageComparator(). Started PackVoltageEncoder()

--- components/bms/src/CANbus_functions.c
+++ components/bms/src/CANbus_functions.c
@@ -6,19 +6,61 @@
 BTM_PackData_t PH_PACKDATA;
 
 //function prototypes
-void CANinfoPullAndFormat();
+void CANinfoPullAndFormatSeries623();
+void VoltageComparator(uint16_t * pMinVoltage, uint16_t * pMaxVoltage);
+void packVoltageEncoder(unsigned int pack_voltage);
 
 
 //function definitions
 void CANinfoPullAndFormatSeries623(){
-  unsigned int packVoltage = PH_PACKDATA.packvoltage;
-  uint16_t MinVtg = 1;
-  uint16_t MaxVtg = 1;
+  unsigned int packVoltage = 0;
+  uint16_t MinVtg = 0;
+  uint16_t MaxVtg = 0;
+  uint8_t aData_series623[8] = { 0 };
 
   //placeholder code, to see the format of struct calls.
   //Looks really clean
   //retreving voltage of module 12 of stack 3 (note the off-by-one array index)
   uint16_t PH_ModuleVOLTAGE = PH_PACKDATA.stack[2].module[11].voltage;
 
+  //gather min and max voltages
+  VoltageComparator(&MinVtg, &MaxVtg);
 
+  /**
+  pack Voltage
+  format:
+    unsigned, 0 to 65 kV -> ASSUMING decimal values from 0 to 65 000
+    because it's alloted 2 bytes in the data frame
+  */
+  packVoltage = packVoltageEncoder(PH_PACKDATA.pack_voltage);
+
+}
+
+void VoltageComparator(uint16_t * pMinVoltage, uint16_t * pMaxVoltage){
+  uint16_t
+    localMinVolt = 1000000, //note that the raw register readings are decimal-shifted to avoid storing floating points
+    localMaxVolt = 0;
+
+  //combines the minVolt and maxVolt loops to reduce redundant struct pulls.
+  for(int i = 0; i < BTM_NUM_DEVICES; i++){
+    for(int j = 0; j < BTM_NUM_MODULES; j++){
+
+      localVoltage = PH_PACKDATA.stack[i].module[j].voltage;
+
+      if(localVoltage < localMinVolt)
+        localMinVolt = localVoltage;
+
+      if(localVoltage > localMaxVolt)
+        localMaxVolt = localVoltage;
+    }
+  }
+
+  //"return" min and max voltage
+  *pMinVoltage = localMinVolt;
+  *pMaxVoltage = localMaxVolt;
+
+}
+
+unsigned int packVoltageEncoder(pack_voltage){
+  return 1337;
 }
