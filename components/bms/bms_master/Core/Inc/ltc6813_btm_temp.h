0e688f84803b6152697ccb935177efac7b928063
a2k-hanlon <andrewkhanlon@gmail.com>
2020-08-18 22:26:28 -0700

    Added header file for ltc6813_btm_temp.h

---
+++ components/bms/bms_master/Core/Inc/ltc6813_btm_temp.h
@@ -0,0 +1,18 @@
+/*
+ * ltc6813_btm_temp.h
+ *
+ *  Created on: Aug. 18, 2020
+ *      Author: Andrew Hanlon
+ */
+
+#ifndef INC_LTC6813_BTM_TEMP_H_
+#define INC_LTC6813_BTM_TEMP_H_
+
+#include "ltc6811_btm.h"
+
+/*============================================================================*/
+/* FUNCTION PROTOTYPES */
+
+BTM_Status_t BTM_TEMP_measureState(BTM_PackData_t* pack);
+
+#endif /* INC_LTC6813_BTM_TEMP_H_ */
