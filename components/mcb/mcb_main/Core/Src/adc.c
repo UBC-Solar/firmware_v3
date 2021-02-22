6cc2a8d6db634abb6a616438ee47c607634a4e55
mihirnimgade <mihir.nim@outlook.com>
2021-02-22 09:47:29 +0530

    Replace previous ADC code with HAL function calls

--- components/mcb/mcb_main/Core/Src/adc.c
+++ components/mcb/mcb_main/Core/Src/adc.c
@@ -1,57 +1,131 @@
-#include "ADC.h"
-
-// 0 - 300 hex pot
-
 /**
- * Initializes an ADC for the regenerative braking toggle
- */
-void ADCInit(void){
+  ******************************************************************************
+  * @file    adc.c
+  * @brief   This file provides code for the configuration
+  *          of the ADC instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
 
-	// Init PA0 to analog input (0x0)
-	GPIOA->CRL &= ~(0xFUL);
+/* Includes ------------------------------------------------------------------*/
+#include "adc.h"
 
-	// Configure ADC2
-	// Enable ADC clock
-	RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
+/* USER CODE BEGIN 0 */
 
-	// Change control registers
-	// By default, CR1 is set correctly
-	ADC2->CR2 |= ADC_CR2_CONT;
+/* USER CODE END 0 */
 
-	// Change the sample time register 2
-	// sets a sample time of 239.5 cycles for channel 0 of the ADCs
-	ADC2->SMPR2 |= 0x7UL;
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
 
-	// Start ADC
-	ADC2->CR2 |= ADC_CR2_ADON;
-	ADC2->CR2    |= 0x8UL;
-	while ((ADC2->CR2 >> 3) & 0x1UL);
-	ADC2->CR2    |= 0x4UL;
-	while ((ADC2->CR2 >> 2) & 0x1UL);
-	ADC2->CR2 |= 0x1UL;
+/* ADC1 init function */
+void MX_ADC1_Init(void)
+{
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
 }
 
-/**
- * Reads a value from the ADC
- */
-uint16_t ReadADC(void){
+void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
+{
 
-	uint16_t ADC_reading = ADC2->DR;
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(adcHandle->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspInit 0 */
 
-	//SendString("    Raw ADC Reading:");
-	//SendInt(ADC_reading);
+  /* USER CODE END ADC1_MspInit 0 */
+    /* ADC1 clock enable */
+    __HAL_RCC_ADC1_CLK_ENABLE();
 
-	/*
-	if (ADC_reading > ADC_ZERO_THRESHOLD)
-	{
-		ADC_reading = 0x000;
-	}
-	else
-	{
-		ADC_reading = ADC_ZERO_THRESHOLD - ADC_reading;
-	}
-	*/
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**ADC1 GPIO Configuration
+    PA0-WKUP     ------> ADC1_IN0
+    */
+    GPIO_InitStruct.Pin = REGEN_VAL_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(REGEN_VAL_GPIO_Port, &GPIO_InitStruct);
 
-	return ADC_reading;
+    /* ADC1 DMA Init */
+    /* ADC1 Init */
+    hdma_adc1.Instance = DMA1_Channel1;
+    hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_adc1.Init.Mode = DMA_NORMAL;
+    hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(adcHandle,DMA_Handle,hdma_adc1);
+
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+  }
 }
+
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
+{
+
+  if(adcHandle->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+  /* USER CODE END ADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC1_CLK_DISABLE();
+
+    /**ADC1 GPIO Configuration
+    PA0-WKUP     ------> ADC1_IN0
+    */
+    HAL_GPIO_DeInit(REGEN_VAL_GPIO_Port, REGEN_VAL_Pin);
+
+    /* ADC1 DMA DeInit */
+    HAL_DMA_DeInit(adcHandle->DMA_Handle);
+  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+  /* USER CODE END ADC1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
