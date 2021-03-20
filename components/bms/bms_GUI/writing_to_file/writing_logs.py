d765dfcd1de66b08810c84a9eb14f471438b8949
abooodeee <47045479+abooodeee@users.noreply.github.com>
2021-03-20 00:22:18 -0700

    Add files via upload

---
+++ components/bms/bms_GUI/writing_to_file/writing_logs.py
@@ -0,0 +1,25 @@
+
+
+from datetime import datetime
+
+def generate_file_name():
+
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y-%H-%M-%S")
+    return dt_string
+
+def write_log(text, path_name = ""):
+
+    if path_name=="":
+        path_name = "logs/" + generate_file_name()
+
+    text=generate_file_name()+" " + text #adding time stamp
+    file = open(path_name, "a+")
+
+    file.write(text)
+
+    file.close()
+if __name__ == '__main__':
+    #debugging
+    for i in range(100):
+        write_log(str(i)+"\n")
