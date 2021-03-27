45d98d1cb34870ce85f7c53340708ea71b29f2f8
matthewe25 <matthewegan00@gmail.com>
2021-03-26 23:17:57 -0400

    fixed module generator file path issue

---
+++ components/bms/bms_master/test/test_selftest.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "selftest.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_selftest_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement selftest");
+}
+
+#endif // TEST
