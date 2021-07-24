cc8d72ae90398443bd0cfc04890487f6ce7fde49
mihirnimgade <mihir.nim@outlook.com>
2021-07-24 00:38:13 -0600

    Reindent file

--- components/mcb/mcb_main/Core/Src/can.c
+++ components/mcb/mcb_main/Core/Src/can.c
@@ -1,21 +1,21 @@
 /**
-  ******************************************************************************
-  * @file    can.c
-  * @brief   This file provides code for the configuration
-  *          of the CAN instances.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    can.c
+ * @brief   This file provides code for the configuration
+ *          of the CAN instances.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "can.h"
@@ -32,96 +32,95 @@ CAN_HandleTypeDef hcan;
 void MX_CAN_Init(void)
 {
 
-  hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 16;
-  hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
-  hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = DISABLE;
-  hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = DISABLE;
-  hcan.Init.ReceiveFifoLocked = DISABLE;
-  hcan.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    hcan.Instance = CAN1;
+    hcan.Init.Prescaler = 16;
+    hcan.Init.Mode = CAN_MODE_NORMAL;
+    hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+    hcan.Init.TimeSeg1 = CAN_BS1_1TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
+    hcan.Init.TimeTriggeredMode = DISABLE;
+    hcan.Init.AutoBusOff = DISABLE;
+    hcan.Init.AutoWakeUp = DISABLE;
+    hcan.Init.AutoRetransmission = DISABLE;
+    hcan.Init.ReceiveFifoLocked = DISABLE;
+    hcan.Init.TransmitFifoPriority = DISABLE;
+    if (HAL_CAN_Init(&hcan) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
 }
 
 void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspInit 0 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if(canHandle->Instance==CAN1)
+    {
+        /* USER CODE BEGIN CAN1_MspInit 0 */
 
-  /* USER CODE END CAN1_MspInit 0 */
-    /* CAN1 clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
+        /* USER CODE END CAN1_MspInit 0 */
+        /* CAN1 clock enable */
+        __HAL_RCC_CAN1_CLK_ENABLE();
 
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**CAN GPIO Configuration
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_8;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**CAN GPIO Configuration
+          PB8     ------> CAN_RX
+          PB9     ------> CAN_TX
+          */
+        GPIO_InitStruct.Pin = GPIO_PIN_8;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        GPIO_InitStruct.Pin = GPIO_PIN_9;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    __HAL_AFIO_REMAP_CAN1_2();
+        __HAL_AFIO_REMAP_CAN1_2();
 
-  /* USER CODE BEGIN CAN1_MspInit 1 */
+        /* USER CODE BEGIN CAN1_MspInit 1 */
 
-  /* USER CODE END CAN1_MspInit 1 */
-  }
+        /* USER CODE END CAN1_MspInit 1 */
+    }
 }
 
 void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 {
 
-  if(canHandle->Instance==CAN1)
-  {
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
+    if(canHandle->Instance==CAN1)
+    {
+        /* Peripheral clock disable */
+        __HAL_RCC_CAN1_CLK_DISABLE();
 
-    /**CAN GPIO Configuration
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX
-    */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
-  }
+        /**CAN GPIO Configuration
+          PB8     ------> CAN_RX
+          PB9     ------> CAN_TX
+          */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
+    }
 }
 
 /* USER CODE BEGIN 1 */
 
 void CAN_Filter_Init(void)
 {
-  // in this case we are using two 16-bit filters in identifer mask mode
-  // therefore, the high and low values for the FilterID and FilterMask are going to be the same since we're just filtering
-	// for one ID (0x626)
-  // 0xC4C0 is chosen since its first 11-bits are 0x626 which is the relevant CAN identifier for the battery SOC message
-  hcan_filter.FilterIdHigh = (uint32_t) (0x626 << 5);
-  hcan_filter.FilterIdLow = (uint32_t) (0x626 << 5);
+    // in this case we are using two 16-bit filters in identifer mask mode
+    // therefore, the high and low values for the FilterID and FilterMask are going to be the same since we're just filtering
+    // for one ID (0x626)
+    hcan_filter.FilterIdHigh = (uint32_t) (0x626 << 5);
+    hcan_filter.FilterIdLow = (uint32_t) (0x626 << 5);
 
-  // masks away the last 5 bits - the only relevant bits are [15:5] (11-bit identifier)
-  hcan_filter.FilterMaskIdHigh = (uint32_t) (0x7FF << 5);
-  hcan_filter.FilterMaskIdLow = (uint32_t) (0x7FF << 5);
+    // masks away the last 5 bits - the only relevant bits are [15:5] (11-bit identifier)
+    hcan_filter.FilterMaskIdHigh = (uint32_t) (0x7FF << 5);
+    hcan_filter.FilterMaskIdLow = (uint32_t) (0x7FF << 5);
 
-  hcan_filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-  hcan_filter.FilterBank = (uint32_t) 0;
-  hcan_filter.FilterMode = CAN_FILTERMODE_IDMASK;
-  hcan_filter.FilterScale = CAN_FILTERSCALE_16BIT;
-  hcan_filter.FilterActivation = CAN_FILTER_ENABLE;
+    hcan_filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+    hcan_filter.FilterBank = (uint32_t) 0;
+    hcan_filter.FilterMode = CAN_FILTERMODE_IDMASK;
+    hcan_filter.FilterScale = CAN_FILTERSCALE_16BIT;
+    hcan_filter.FilterActivation = CAN_FILTER_ENABLE;
 }
 
 /* USER CODE END 1 */
