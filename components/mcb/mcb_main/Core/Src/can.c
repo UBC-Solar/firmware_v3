4ef679a801ec246ea0c9785380d7c98344e70bf8
mihirnimgade <mihir.nim@outlook.com>
2021-09-24 16:12:45 -0700

    Add documentation for CAN structs and functions

--- components/mcb/mcb_main/Core/Src/can.c
+++ components/mcb/mcb_main/Core/Src/can.c
@@ -2,14 +2,19 @@
   ******************************************************************************
   * @file    can.c
   * @brief   This file provides code for the configuration
-  *          of the CAN instances.
+  *          of the CAN instances and CAN filters.
   ******************************************************************************
   */
 
+/* Includes ------------------------------------------------------------------*/
 #include "can.h"
 
 /* USER CODE BEGIN 0 */
 
+/**
+ * 	CAN message header for a drive command. This command header is used in normal, regen, and
+ * 	cruise control mode to send appropriate drive command to the motor controller.
+ */
 CAN_TxHeaderTypeDef drive_command_header = {
     .StdId = DRIVER_CONTROLS_BASE_ADDRESS + 1,
     .ExtId = 0x0000,
@@ -17,102 +22,114 @@ CAN_TxHeaderTypeDef drive_command_header = {
     .RTR = CAN_RTR_DATA,
     .DLC = CAN_DATA_LENGTH};
 
+/**
+ * 	CAN message header for a next screen command. This command header is used to indicate to the
+ * 	driver information display (DID) board that the next screen button has been pressed.
+ */
 CAN_TxHeaderTypeDef screen_cruise_control_header = {
-  .StdId = DRIVER_CONTROLS_BASE_ADDRESS,
-  .ExtId = 0x0000,
-  .IDE = CAN_ID_STD,
-  .RTR = CAN_RTR_DATA,
-  .DLC = CAN_CONTROL_DATA_LENGTH
-}
+	.StdId = DRIVER_CONTROLS_BASE_ADDRESS + 3,
+	.ExtId = 0x0000,
+	.IDE = CAN_ID_STD,
+	.RTR = CAN_RTR_DATA,
+	.DLC = CAN_CONTROL_DATA_LENGTH
+};
 
-CAN_RxHeaderTypeDef can_rx_header;
-CAN_FilterTypeDef battery_soc_filter;
+CAN_RxHeaderTypeDef can_rx_header;				/**< Stores the header information for CAN messages read from
+													 the RX (receive) buffer */
 
-uint32_t can_mailbox;
+CAN_FilterTypeDef battery_soc_filter;			/**< Stores the intialisation information for the hardware CAN
+													 that filters for battery (ID 0x626) messages */
+
+uint32_t can_mailbox;							/**< Stores the TX (transmit) mailbox that a CAN message has
+													 been placed into.*/
 
 /* USER CODE END 0 */
 
-CAN_HandleTypeDef hcan;
+CAN_HandleTypeDef hcan;							/**< CAN peripheral handle. */
 
-/* CAN init function */
+/**
+ *  @brief 	CAN initialization function.
+ *  */
 void MX_CAN_Init(void)
 {
-    hcan.Instance = CAN1;
-    hcan.Init.Prescaler = 16;
-    hcan.Init.Mode = CAN_MODE_NORMAL;
-    hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-    hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
-    hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
-    hcan.Init.TimeTriggeredMode = ENABLE;
-    hcan.Init.AutoBusOff = ENABLE;
-    hcan.Init.AutoWakeUp = ENABLE;
-    hcan.Init.AutoRetransmission = DISABLE;
-    hcan.Init.ReceiveFifoLocked = DISABLE;
-    hcan.Init.TransmitFifoPriority = DISABLE;
-    if (HAL_CAN_Init(&hcan) != HAL_OK)
-    {
-        Error_Handler();
-    }
+  hcan.Instance = CAN1;
+  hcan.Init.Prescaler = 16;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan.Init.TimeTriggeredMode = ENABLE;
+  hcan.Init.AutoBusOff = ENABLE;
+  hcan.Init.AutoWakeUp = ENABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-void HAL_CAN_MspInit(CAN_HandleTypeDef *canHandle)
+void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
 {
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if (canHandle->Instance == CAN1)
-    {
-        /* CAN1 clock enable */
-        __HAL_RCC_CAN1_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspInit 0 */
 
-        __HAL_RCC_GPIOB_CLK_ENABLE();
-        /**CAN GPIO Configuration
-                PB8     ------> CAN_RX
-                PB9     ------> CAN_TX
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_8;
-        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /* USER CODE END CAN1_MspInit 0 */
+    /* CAN1 clock enable */
+    __HAL_RCC_CAN1_CLK_ENABLE();
 
-        GPIO_InitStruct.Pin = GPIO_PIN_9;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**CAN GPIO Configuration
+    PB8     ------> CAN_RX
+    PB9     ------> CAN_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        __HAL_AFIO_REMAP_CAN1_2();
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        /* USER CODE BEGIN CAN1_MspInit 1 */
+    __HAL_AFIO_REMAP_CAN1_2();
 
-        /* USER CODE END CAN1_MspInit 1 */
-    }
+  }
 }
 
-void HAL_CAN_MspDeInit(CAN_HandleTypeDef *canHandle)
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 {
 
-    if (canHandle->Instance == CAN1)
-    {
-        /* USER CODE BEGIN CAN1_MspDeInit 0 */
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspDeInit 0 */
 
-        /* USER CODE END CAN1_MspDeInit 0 */
-        /* Peripheral clock disable */
-        __HAL_RCC_CAN1_CLK_DISABLE();
+  /* USER CODE END CAN1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_CAN1_CLK_DISABLE();
 
-        /**CAN GPIO Configuration
-            PB8     ------> CAN_RX
-            PB9     ------> CAN_TX
-        */
-        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8 | GPIO_PIN_9);
+    /**CAN GPIO Configuration
+    PB8     ------> CAN_RX
+    PB9     ------> CAN_TX
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
 
-        /* USER CODE BEGIN CAN1_MspDeInit 1 */
+  /* USER CODE BEGIN CAN1_MspDeInit 1 */
 
-        /* USER CODE END CAN1_MspDeInit 1 */
-    }
+  /* USER CODE END CAN1_MspDeInit 1 */
+  }
 }
 
-/* USER CODE BEGIN 1 */
 
-// TODO: add brief here
+/**
+ * 	@brief 	Initializes a hardware CAN filter that filters for CAN messages that have the standard ID 0x626.
+ * 			This corresponds to a message from the BMS (battery management system) that contains the battery SOC.
+ */
 void CAN_Filter_Init(void)
 {
     // in this case we are using two 16-bit filters in identifer mask mode
