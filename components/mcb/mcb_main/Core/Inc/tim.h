4926cff2279f13133e2fa8023d3fce692ae3fcb9
Patrick Gousseau <patrickg@ensemble.com>
2022-04-09 10:25:51 -0700

    replaced encoder drivers with hal

--- components/mcb/mcb_main/Core/Inc/tim.h
+++ components/mcb/mcb_main/Core/Inc/tim.h
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file    can.h
+  * @file    tim.h
   * @brief   This file contains all the function prototypes for
-  *          the can.c file
+  *          the tim.c file
   ******************************************************************************
   * @attention
   *
@@ -17,8 +17,8 @@
   ******************************************************************************
   */
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __CAN_H__
-#define __CAN_H__
+#ifndef __TIM_H__
+#define __TIM_H__
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,31 +31,17 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern CAN_HandleTypeDef hcan;
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN Private defines */
 
-#define DRIVER_CONTROLS_BASE_ADDRESS 0x400
-#define MOTOR_CTRL_BASE 0x500
-#define BATTERY_BASE 0x620
-
-#define CAN_DATA_LENGTH 8 			// incoming data is 64 bytes total
-#define CAN_HALF_DATA_LENGTH 4 		// some IDs split the data into 2 values of 32 byte (e.g., 0x50B)
-#define CAN_CONTROL_DATA_LENGTH 2
-
-extern CAN_TxHeaderTypeDef drive_command_header;
-extern CAN_TxHeaderTypeDef screen_cruise_control_header;
-extern CAN_RxHeaderTypeDef can_rx_header;
-extern CAN_FilterTypeDef mcb_filter;
-extern uint32_t can_mailbox;
-
 /* USER CODE END Private defines */
 
-void MX_CAN_Init(void);
+void MX_TIM1_Init(void);
+void MX_TIM3_Init(void);
 
 /* USER CODE BEGIN Prototypes */
-
-void CAN_Filter_Init(void);
 
 /* USER CODE END Prototypes */
 
@@ -63,6 +49,6 @@ void CAN_Filter_Init(void);
 }
 #endif
 
-#endif /* __CAN_H__ */
+#endif /* __TIM_H__ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
