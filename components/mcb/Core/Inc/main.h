c479dab58bf6269ad9d6261443d3b1aec1b9eeb5
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-06-12 23:24:58 -0700

    Added components folder

---
+++ components/mcb/Core/Inc/main.h
@@ -0,0 +1,105 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ADC_Throttle_Pin GPIO_PIN_0
+#define ADC_Throttle_GPIO_Port GPIOA
+#define ADC_Regen_Pin GPIO_PIN_1
+#define ADC_Regen_GPIO_Port GPIOA
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define MECH_BRAKE_Pin GPIO_PIN_1
+#define MECH_BRAKE_GPIO_Port GPIOB
+#define SWITCH_REVERSE_Pin GPIO_PIN_2
+#define SWITCH_REVERSE_GPIO_Port GPIOB
+#define SWITCH_REGEN_EN_Pin GPIO_PIN_10
+#define SWITCH_REGEN_EN_GPIO_Port GPIOB
+#define SWITCH_PARK_Pin GPIO_PIN_11
+#define SWITCH_PARK_GPIO_Port GPIOB
+#define BTN_CRUISE_TOGGLE_Pin GPIO_PIN_13
+#define BTN_CRUISE_TOGGLE_GPIO_Port GPIOB
+#define BTN_CRUISE_TOGGLE_EXTI_IRQn EXTI15_10_IRQn
+#define BTN_CRUISE_UP_Pin GPIO_PIN_14
+#define BTN_CRUISE_UP_GPIO_Port GPIOB
+#define BTN_CRUISE_UP_EXTI_IRQn EXTI15_10_IRQn
+#define BTN_CRUISE_DOWN_Pin GPIO_PIN_15
+#define BTN_CRUISE_DOWN_GPIO_Port GPIOB
+#define BTN_CRUISE_DOWN_EXTI_IRQn EXTI15_10_IRQn
+#define BTN_NEXT_PAGE_Pin GPIO_PIN_6
+#define BTN_NEXT_PAGE_GPIO_Port GPIOC
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ components/mcb/Core/Src/MCB.c
@@ -0,0 +1,68 @@
+/*
+ * MCB.c
+ *
+ *  Created on: Jun. 1, 2023
+ *      Author: Kyle Groulx
+ */
+
+#include <mcb.h>
+
+InputFlags input_flags; 	   // Event flags for deciding what state to be in.
+enum DriveState state;		   // Holds the current the drive state.
+float cruise_velocity = 0; 	   // Velocity for cruise control
+float velocity_of_car = 0; 	   // Current velocity of the car will be stored here.
+uint8_t battery_soc = 0; 	   // Stores the charge of the battery, updated in a task.
+
+
+
+/*
+ *   Takes current value and velocity float value and sends in via CAN as an array of bytes.
+ */
+void SendCANMotorCommand(float current, float velocity)
+{
+	union FloatBytes c;
+	union FloatBytes v;
+
+	c.float_value = current;
+	v.float_value = velocity;
+
+	uint8_t data_send[CAN_DATA_LENGTH];
+	for (int i = 0; i < (uint8_t) CAN_DATA_LENGTH / 2; i++)
+	{
+		data_send[i] = v.bytes[i];
+	    data_send[4 + i] = c.bytes[i];
+	}
+	HAL_CAN_AddTxMessage(&hcan, &drive_command_header, data_send, &can_mailbox);
+}
+
+/*
+ *  Function used for normalizing(0-1) and accounting for deadzone of ADC inputs.
+ */
+float NormalizeADCValue(uint16_t value)
+{
+	return (value - ADC_DEADZONE >= 0 ? ((float)(value - ADC_DEADZONE))/(ADC_MAX - ADC_DEADZONE) : 0.0);
+}
+
+/*
+ *  Function used for sending the Next Page command to DID.
+ */
+void SendCANDIDNextPage()
+{
+	//Todo Check BOM for this CAN message
+	uint8_t data_send[CAN_DATA_LENGTH];
+	HAL_CAN_AddTxMessage(&hcan, &drive_command_header, data_send, &can_mailbox);
+}
+
+void UpdateInputFlags(InputFlags* flags)
+{
+	flags->mech_brake_pressed = HAL_GPIO_ReadPin(MECH_BRAKE_GPIO_Port, MECH_BRAKE_Pin);
+	flags->park_enabled = HAL_GPIO_ReadPin(SWITCH_PARK_GPIO_Port, SWITCH_PARK_Pin);
+	flags->reverse_enabled = HAL_GPIO_ReadPin(SWITCH_REVERSE_GPIO_Port, SWITCH_REVERSE_Pin);
+	flags->regen_enabled = HAL_GPIO_ReadPin(SWITCH_REGEN_EN_GPIO_Port, SWITCH_REGEN_EN_Pin);
+
+	flags->velocity_under_threshold = (velocity_of_car < MIN_REVERSE_VELOCITY);
+	flags->charge_under_threshold = (battery_soc < BATTERY_SOC_THRESHOLD);
+}
+
+
+
