2c10d40037f4018cd8a655810740f7babdad8110
Kyle Groulx <87883655+kcgroulx@users.noreply.github.com>
2023-11-27 17:21:21 -0800

    fix: add code for each drive state

--- components/mcb/mcb_main/Core/Src/mcb.c
+++ components/mcb/mcb_main/Core/Src/mcb.c
@@ -7,14 +7,10 @@
 
 #include "mcb.h"
 
-bool gBatteryRegenEnabled = false;
-bool gBatterySOCUnderThreshold = false;
-
-float gCruiseVelocity = 0; 	   // Velocity for cruise control
-float gVelocityOfCar = 0; 	   // Current velocity of the car will be stored here.
-uint8_t gBatterySOC = 0; 	   // Stores the charge of the battery, updated in a task.
+float cruiseVelocity = 0.0;		// Velocity for cruise control
+float velocityOfCar = 0.0;		// Current velocity of the car will be stored here.
 InputFlags input_flags;
-DriveState state;
+DriveState state = PARK;
 
 /*
  *	Main state machine task
@@ -24,223 +20,202 @@ void TaskMCBStateMachine()
 	for(;;)
 	{
 		taskENTER_CRITICAL();
-
-		float velocity = 0.0;
-		float current = 0.0;
-		uint16_t ADC_throttle_val = ReadADC(&hadc1);
+		MotorCommand motorCommand;
+		UpdateInputFlags(&input_flags);
 
 		switch(state)
 		{
 		case DRIVE:
-			current = NormalizeADCValue(ADC_throttle_val);
-
-			/*
-			 * 	Regen Enabled
-			 */
-			if ( input_flags.regen_enabled && input_flags.battery_SOC_under_threshold && input_flags.battery_temp_under_threshold )
-			{
-				if( current == 0.0 )
-				{
-					velocity = 0.0;
-				}
-				else if( current > 0.0 )
-				{
-					velocity = 100.0;
-				}
-			}
-			else // Regen disabled
-			{
-				#ifdef MITSUBA
-				if(current == 0.0)
-				{
-					velocity = 0.0;
-					current = 0.0;
-				}
-				else if(current < P1)
-				{
-					velocity = 100.0;
-					current = P1;
-				}
-				else if(current > P1)
-				{
-					velocity = 100.0;
-				}
-				#endif
-				#ifdef TRITIUM
-				if(current == 0.0)
-				{
-					velocity = 0.0;
-				}
-				else if(current > 0.0)
-				{
-					velocity = 100.0;
-				}
-				#endif
-			}
-			break;
-
-			/*
-			 *  State transitions
-			 */
-			if( input_flags.reverse_enabled && input_flags.velocity_under_threshold )
-			{
-				state = REVERSE;
-			}
-			else if( input_flags.park_enabled && input_flags.velocity_under_threshold )
-			{
-				state = PARK;
-			}
-			else if( input_flags.cruise_enabled )
-			{
-				state = CRUISE;
-			}
+			motorCommand = DoStateDRIVE(input_flags);
+		break;
 
 		case CRUISE:
-			/*
-			 *  State transitions
-			 */
-			if ( input_flags.mech_brake_pressed || !input_flags.cruise_enabled)
-			{
-				state = DRIVE;
-			}
-			break;
+			motorCommand = DoStateCRUISE(input_flags);
+		break;
 
-		case REVERSE: // Regen disabled in REVERSE
-			#ifdef MITSUBA
-			if(current == 0.0)
-			{
-				velocity = 0.0;
-				current = 0.0;
-			}
-			else if(current < P1)
-			{
-				velocity = -100.0;
-				current = P1;
-			}
-			else if(current > P1)
-			{
-				velocity = -100.0;
-			}
-			#endif
-			#ifdef TRITIUM
-			if(current == 0.0)
-			{
-				velocity = 0.0;
-			}
-			else if(current > 0.0)
-			{
-				velocity = -100.0;
-			}
-			#endif
-
-			/*
-			 *  State transitions
-			 */
-			if ( input_flags.drive_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
-			{
-				state = DRIVE;
-			}
-			break;
+		case REVERSE:
+			motorCommand = DoStateREVERSE(input_flags);
+		break;
 
 		case PARK:
-			velocity = 0.0;
-			current = 1.0;
-			/*
-			 *  State transitions
-			 */
-			if ( input_flags.park_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
-			{
-				state = DRIVE;
-			}
-			break;
+			motorCommand = DoStatePARK(input_flags);
+		break;
 
 		default:
-			velocity = 0.0;
-			current = 1.0;
+			motorCommand.velocity = 0.0;
+			motorCommand.throttle = 0.0;
 		}
 
-
-		SendCANMotorCommand(current, velocity);
-
-
-
-
-
-
-
-		/*
-		// Set input flags
-		input_flags.throttle_pressed = ADC_throttle_val > ADC_DEADZONE;
-		input_flags.cruise_accelerate_enabled = NormalizeADCValue(ADC_throttle_val) > CRUISE_CURRENT;
-		input_flags.regen_enabled = (gBatterySOC < BATTERY_SOC_THRESHOLD) && gBatteryRegenEnabled && HAL_GPIO_ReadPin(REGEN_EN_GPIO_Port, REGEN_EN_Pin);
-
-		if(ADC_regen_val > ADC_DEADZONE)
-			input_flags.cruise_enabled = false;
-
-		input_flags.mech_brake_pressed = HAL_GPIO_ReadPin(BRK_IN_GPIO_Port, BRK_IN_Pin);
-		input_flags.park_enabled = HAL_GPIO_ReadPin(PARK_EN_GPIO_Port, PARK_EN_Pin);
-		input_flags.reverse_enabled = HAL_GPIO_ReadPin(RVRS_EN_GPIO_Port, RVRS_EN_Pin);
-		input_flags.velocity_under_threshold = (gVelocityOfCar < MIN_REVERSE_VELOCITY);
-
-		// Calculate state
-		if (input_flags.park_enabled && input_flags.velocity_under_threshold)
-			state = PARK;
-		else if (input_flags.mech_brake_pressed)
-			state = IDLE;
-		else if (input_flags.regen_enabled)
-		  	state = REGEN;
-		else if (input_flags.cruise_enabled && input_flags.cruise_accelerate_enabled)
-		  	state = CRUISE_ACCELERATE;
-		else if (input_flags.cruise_enabled)
-		    state = CRUISE;
-		else if (input_flags.reverse_enabled && input_flags.velocity_under_threshold)
-			state = REVERSE;
-		else if (input_flags.throttle_pressed)
-		    state = DRIVE;
-		else
-		  	state = IDLE;
-
-		// Calculate velocity and current based on input_flags
-		if(state == PARK)
-		{
-			velocity = 0.0;
-			current = 1.0;
-		}
-		else if(state == REGEN)
-	    {
-	    	velocity = 0.0;
-	    	current = NormalizeADCValue(ADC_regen_val);
-	    }
-	    else if(state == DRIVE)
-	    {
-	    	velocity = 100.0;
-	    	current = NormalizeADCValue(ADC_throttle_val);
-	    }
-	    else if(state == REVERSE)
-	    {
-	    	velocity = -100.0;
-	    	current = NormalizeADCValue(ADC_throttle_val);
-	    }
-	    else if (state == CRUISE)
-	    {
-	    	velocity = gCruiseVelocity;
-	    	current = 1.0;
-	    }
-		else if (state == CRUISE_ACCELERATE)
-		{
-			velocity = 100.0;
-			current = NormalizeADCValue(ADC_throttle_val);
-		}
-
-		// Send CAN message to motor controller
-		SendCANMotorCommand(velocity, current);
-		*/
-
+		SendCANMotorCommand(motorCommand);
 
 		taskEXIT_CRITICAL();
 		osDelay(DELAY_MCB_STATE_MACHINE);
 	}
 }
+
+
+MotorCommand DoStateDRIVE( InputFlags input_flags )
+{
+	MotorCommand motorCommand;
+
+	uint16_t ADC_throttle_val = ReadADC(&hadc1);
+	motorCommand.throttle = NormalizeADCValue(ADC_throttle_val);
+
+	if ( input_flags.mech_brake_pressed )
+	{
+		motorCommand.velocity = 0.0;
+		motorCommand.throttle = 0.0;
+	}
+	else if ( input_flags.regen_enabled && input_flags.battery_SOC_under_threshold && input_flags.battery_temp_under_threshold )
+	{
+		if( motorCommand.throttle == 0.0 )
+		{
+			motorCommand.velocity = 0.0;
+		}
+		else if( motorCommand.throttle > 0.0 )
+		{
+			motorCommand.velocity = 100.0;
+		}
+	}
+	else // Regen disabled
+	{
+		#ifdef MITSUBA
+			if(motorCommand.throttle == 0.0)
+			{
+				motorCommand.velocity = 0.0;
+				motorCommand.throttle = 0.0;
+			}
+			else if(motorCommand.throttle < P1)
+			{
+				motorCommand.velocity = 100.0;
+				motorCommand.throttle = P1;
+			}
+			else if(motorCommand.throttle > P1)
+			{
+				motorCommand.velocity = 100.0;
+			}
+		#endif
+		#ifdef TRITIUM
+			if(motorCommand.throttle == 0.0)
+			{
+				motorCommand.velocity = 0.0;
+			}
+			else if(motorCommand.throttle > 0.0)
+			{
+				motorCommand.velocity = 100.0;
+			}
+		#endif
+	}
+
+	/*
+	 *  State transitions
+	 */
+	if( input_flags.reverse_enabled && input_flags.velocity_under_threshold )
+	{
+		state = REVERSE;
+	}
+	else if( input_flags.park_enabled && input_flags.velocity_under_threshold )
+	{
+		state = PARK;
+	}
+	else if( input_flags.cruise_enabled )
+	{
+		state = CRUISE;
+	}
+
+	return motorCommand;
+}
+
+
+MotorCommand DoStateCRUISE(InputFlags input_flags)
+{
+	MotorCommand motorCommand;
+
+	motorCommand.velocity = cruiseVelocity;
+	motorCommand.throttle = CRUISE_THROTTLE;
+	/*
+	 *  State transitions
+	 */
+	if ( input_flags.mech_brake_pressed || !input_flags.cruise_enabled)
+	{
+		state = DRIVE;
+	}
+	return motorCommand;
+}
+
+MotorCommand DoStateREVERSE(InputFlags input_flags)
+{
+	MotorCommand motorCommand;
+	#ifdef MITSUBA
+		if(motorCommand.throttle == 0.0)
+		{
+			motorCommand.velocity = 0.0;
+			motorCommand.throttle = 0.0;
+		}
+		else if(motorCommand.throttle < P1)
+		{
+			motorCommand.velocity = -100.0;
+			motorCommand.throttle = P1;
+		}
+		else if(motorCommand.throttle > P1)
+		{
+			motorCommand.velocity = -100.0;
+		}
+	#endif
+	#ifdef TRITIUM
+		if(motorCommand.throttle == 0.0)
+		{
+			motorCommand.velocity = 0.0;
+		}
+		else if(motorCommand.throttle > 0.0)
+		{
+			motorCommand.velocity = -100.0;
+		}
+	#endif
+
+	/*
+	 *  State transitions
+	 */
+	if ( input_flags.drive_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
+	{
+		state = DRIVE;
+	}
+	else if ( input_flags.park_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
+	{
+		state = PARK;
+	}
+	return motorCommand;
+}
+
+MotorCommand DoStatePARK(InputFlags input_flags)
+{
+	MotorCommand motorCommand;
+	motorCommand.velocity = 0.0;
+	motorCommand.throttle = 1.0;
+
+	/*
+	 *  State transitions
+	 */
+	if ( input_flags.drive_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
+	{
+		state = DRIVE;
+	}
+	else if ( input_flags.reverse_enabled && input_flags.mech_brake_pressed && input_flags.velocity_under_threshold )
+	{
+		state = REVERSE;
+	}
+	return motorCommand;
+}
+
+void UpdateInputFlags(InputFlags * input_flags)
+{
+	input_flags->drive_enabled = !HAL_GPIO_ReadPin(FWRD_EN_GPIO_Port, FWRD_EN_Pin);
+	input_flags->drive_enabled = !HAL_GPIO_ReadPin(RVRS_EN_GPIO_Port, RVRS_EN_Pin);
+	input_flags->drive_enabled = !HAL_GPIO_ReadPin(PARK_EN_GPIO_Port, PARK_EN_Pin);
+	input_flags->mech_brake_pressed = !HAL_GPIO_ReadPin(BRK_IN_GPIO_Port, BRK_IN_Pin);
+	input_flags->regen_enabled = !HAL_GPIO_ReadPin(REGEN_EN_GPIO_Port, REGEN_EN_Pin);
+}
+
 
 void ParseCANBatterySOC(uint8_t * CANMessageData)
 {
@@ -255,13 +230,13 @@ void ParseCANVelocity(uint8_t * CANMessageData)
 	{
 		velocity.bytes[i] = CANMessageData[i + 4]; // Vehicle Velocity is stored in bits 32-63.
 	}
-	gVelocityOfCar = velocity.float_value;
+	velocityOfCar = velocity.float_value;
 	input_flags.velocity_under_threshold = velocity.float_value < VELOCITY_THRESHOLD;
 }
 
 void ParseCANBatteryTemp(uint8_t * CANMessageData)
 {
-	gBatteryRegenEnabled = isBitSetFromArray(CANMessageData, 17); // regen_disable bit is stored in bit 17
+	input_flags.battery_temp_under_threshold = isBitSetFromArray(CANMessageData, 17); // regen_disable bit is stored in bit 17
 }
 
 void TaskGetCANMessage()
@@ -299,13 +274,13 @@ void TaskGetCANMessage()
 /*
  *   Takes current value and velocity float value and sends in via CAN as an array of bytes.
  */
-void SendCANMotorCommand(float current, float velocity)
+void SendCANMotorCommand(MotorCommand motorCommand)
 {
 	union FloatBytes c;
 	union FloatBytes v;
 
-	c.float_value = current;
-	v.float_value = velocity;
+	c.float_value = motorCommand.throttle;
+	v.float_value = motorCommand.velocity;
 
 	uint8_t data_send[CAN_DATA_LENGTH];
 	for (int i = 0; i < (uint8_t) CAN_DATA_LENGTH / 2; i++)
