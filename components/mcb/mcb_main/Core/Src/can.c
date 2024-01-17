141636ce098a64286cf9ad54c640722658edc1f0
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2024-01-16 19:50:45 -0800

    Set CAN baudrate to 500K

--- components/mcb/mcb_main/Core/Src/can.c
+++ components/mcb/mcb_main/Core/Src/can.c
@@ -67,8 +67,8 @@ void MX_CAN_Init(void)
   hcan.Init.Prescaler = 4;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_11TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan.Init.TimeTriggeredMode = DISABLE;
   hcan.Init.AutoBusOff = DISABLE;
   hcan.Init.AutoWakeUp = DISABLE;
