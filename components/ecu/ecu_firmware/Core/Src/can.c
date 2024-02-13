009d1f3daaea4968db06f1af92c5ce9a3758ef72
mjohal67 <majohal67@gmail.com>
2024-02-12 22:36:43 -0800

    fix: bit shifting in 0x450

--- components/ecu/ecu_firmware/Core/Src/can.c
+++ components/ecu/ecu_firmware/Core/Src/can.c
@@ -93,9 +93,9 @@ void CAN_SendMessage450()
     uint8_t lvs_current_rescaled = (uint8_t)((ecu_data.adc_data.ADC_lvs_current * MESSAGE_450_LVS_CURRENT_SCALE_FACTOR) / LVS_CURRENT_LSB_PER_V);
 
     txMessage.data[0] = (uint8_t)batt_current_rescaled;
-    txMessage.data[1] = (uint8_t)(batt_current_rescaled) >> 8;
+    txMessage.data[1] = (uint8_t)(batt_current_rescaled >> 8);
     txMessage.data[2] = (uint8_t)ecu_data.adc_data.ADC_supp_batt_volt;
-    txMessage.data[3] = (uint8_t)(ecu_data.adc_data.ADC_supp_batt_volt) >> 8;
+    txMessage.data[3] = (uint8_t)(ecu_data.adc_data.ADC_supp_batt_volt >> 8);
     txMessage.data[4] = (uint8_t)lvs_current_rescaled;
     txMessage.data[5] = (uint8_t)ecu_data.status.raw;
 
