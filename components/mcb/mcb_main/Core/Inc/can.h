ac6ee1ec6d9df9fc08b977b9fc90a948d1c39f90
mridul2004 <mridul.singh004@gmail.com>
2024-02-20 15:22:11 -0800

    corrected implementation of MCB drive state message

--- components/mcb/mcb_main/Core/Inc/can.h
+++ components/mcb/mcb_main/Core/Inc/can.h
@@ -51,6 +51,7 @@ extern CAN_HandleTypeDef hcan;
 
 extern CAN_TxHeaderTypeDef drive_command_header;
 extern CAN_TxHeaderTypeDef DID_next_page_header;
+extern CAN_TxHeaderTypeDef drive_state_header;
 extern uint32_t can_mailbox;
 extern CAN_RxHeaderTypeDef can_rx_header;
 /* USER CODE END Private defines */
