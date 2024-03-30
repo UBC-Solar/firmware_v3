f5c21101316f790349c19ddd21ac788a596adb18
mjohal67 <majohal67@gmail.com>
2024-03-30 13:58:28 -0700

    feat: add python githash script

--- libraries/github/githash.h
+++ libraries/github/githash.h
@@ -1,8 +1,9 @@
-#ifndef __GITHASH_H__
-#define __GITHASH_H__
+    #ifndef __GITHASH_H__
+    #define __GITHASH_H__
 
-// GITHASH is always 8 chars long and contains the 7 char git hash
-// and either a space or start depending on if the commit is dirty
-#define GITHASH = "xxxxxxxx"
+    // GITHASH is always 8 chars long and contains the 7 char git hash
+    // and either a space or start depending on if the commit is dirty
+    const char[] githash = "c9426a8*"
 
-#endif
+    #endif
+    
