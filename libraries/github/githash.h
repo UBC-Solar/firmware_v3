c9426a8f998ee4b6597088c31cdf48a6a5d9045b
Nicolas Ricci <nicolsaric7@gmail.com>
2024-03-30 13:05:59 -0700

    header files that contain git hash

---
+++ libraries/github/githash.h
@@ -0,0 +1,8 @@
+#ifndef __GITHASH_H__
+#define __GITHASH_H__
+
+// GITHASH is always 8 chars long and contains the 7 char git hash
+// and either a space or start depending on if the commit is dirty
+#define GITHASH = "xxxxxxxx"
+
+#endif
