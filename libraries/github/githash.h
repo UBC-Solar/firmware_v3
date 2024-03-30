9a320bacc1dca68dc2d8d4a4fad4ead7ee9c0396
Nicolas Ricci <nicolsaric7@gmail.com>
2024-03-30 14:07:52 -0700

    set githash

--- libraries/github/githash.h
+++ libraries/github/githash.h
@@ -3,7 +3,7 @@
 
     // GITHASH is always 8 chars long and contains the 7 char git hash
     // and either a space or star depending on if the commit is dirty
-    const char[] githash = "f5212c3*"
+    const char[] githash = "XXXXXXXX"
 
     #endif
     
