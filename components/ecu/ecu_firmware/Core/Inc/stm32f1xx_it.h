1ab95018a9081b84796ff91d31427479dc170d45
Mischa Johal <61034360+mjohal67@users.noreply.github.com>
2024-06-28 18:24:03 -0700

    add: windowed analog watchdog (#140)

--- components/ecu/ecu_firmware/Core/Inc/stm32f1xx_it.h
+++ components/ecu/ecu_firmware/Core/Inc/stm32f1xx_it.h
@@ -61,6 +61,7 @@ void DMA1_Channel1_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
 void USB_HP_CAN1_TX_IRQHandler(void);
 void USB_LP_CAN1_RX0_IRQHandler(void);
+void TIM2_IRQHandler(void);
 void TIM3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
