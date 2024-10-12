5e3d8ca566cd7f3b8b26833e8a49668e1180636a
Aarjavjain101 <Aarjavjain2736@gmail.com>
2024-10-11 23:22:57 -0700

    Refactor: Basic comment changes

--- components/tel/Core/Src/radio.c
+++ components/tel/Core/Src/radio.c
@@ -53,11 +53,11 @@ void increment_to_next_tx_msg()
  */
 void RADIO_send_msg_uart(double timestamp)
 {
-	RADIO_QueueMsg_TypeDef* current_queue_msg_ptr = &sg_rx_queue[sg_tx_queue_index];
+	RADIO_QueueMsg_TypeDef* current_queue_msg_ptr = &sg_rx_queue[sg_tx_queue_index];	// Get the current queue msg
 
 	if (!(current_queue_msg_ptr->is_sent))		
 	{
-		RADIO_Msg_TypeDef* radio_msg = &(current_queue_msg_ptr->radio_msg);
+		RADIO_Msg_TypeDef* radio_msg = &(current_queue_msg_ptr->radio_msg);	// Get the radio message to send
 
 		prep_radio_msg(radio_msg, timestamp);	                        	// Modify various fields for sunlink compatibility
 		UART_radio_transmit(radio_msg);	                                    // Sending to Module via DMA
@@ -65,7 +65,7 @@ void RADIO_send_msg_uart(double timestamp)
 		current_queue_msg_ptr->is_sent = true;	     						// Mark as sent                       
 	}
 
-	increment_to_next_tx_msg();
+	increment_to_next_tx_msg();												// Move to next message to tx
 }
 
 
@@ -102,13 +102,14 @@ void RADIO_set_rx_msg(uint32_t can_id, uint8_t* can_data, uint32_t DLC)
 	if ((current_queue_message->is_sent))		// If the message is not sent, do not overwrite
 	{
 		/* Set all radio message fields minimally */
-		current_queue_message->radio_msg.can_id_reversed = BITOPS_32BIT_REVERSE(can_id);
+		current_queue_message->radio_msg.can_id_reversed = BITOPS_32BIT_REVERSE(can_id);	// Flip for readability and sunlink
 		memcpy(&(current_queue_message->radio_msg.data[START_OF_ARRAY]), &can_data[START_OF_ARRAY], RADIO_DATA_LENGTH);
 		current_queue_message->radio_msg.data_len = DLC & MASK_4_BITS;
-		current_queue_message->is_sent = false;
+
+		current_queue_message->is_sent = false;									// 'Notify' to Tx function to send this message
 
 		sg_tx_queue_index = sg_rx_queue_index;					     		    // Move Tx tracker to Rx Tracker for immediate Tx
-		sg_rx_queue_index = CIRCULAR_INCREMENT_SET(sg_rx_queue_index, MAX_RX_QUEUE_SIZE);		// Move Rx tracker to next position
+		sg_rx_queue_index = CIRCULAR_INCREMENT_SET(sg_rx_queue_index, MAX_RX_QUEUE_SIZE);		// Move Rx tracker to find a free spot
 	}
 	else {}		// If not sent then ERROR. Radio Tx might be too slow
 }
