dce2f15ab0cb02b3dd1f40dfde5790f87dee5b80
Makatendeka Chikumbu <makac1896@gmail.com>
2024-10-30 20:20:17 -0700

    Update canload.h

--- components/tel/Core/Inc/canload.h
+++ components/tel/Core/Inc/canload.h
@@ -1,39 +1,12 @@
-/*
- * canbusload.h
- *
- *  Created on: Oct 17, 2024
- *      Author: makac
- */
-
 #ifndef INC_CANBUSLOAD_H_
 #define INC_CANBUSLOAD_H_
 
 #include "can.h"
 
-#define CANLOAD_BIT_RATE 500000
-#ifndef CANLOAD_WINDOW_SIZE
-    #define CANLOAD_WINDOW_SIZE 5
-#endif
-
-#define CANLOAD_SOF_BITS 1
-#define CANLOAD_STANDARD_ID_BITS 11
-#define CANLOAD_EXTENDED_ID_BITS 29
-#define CANLOAD_SRR_BITS 1
-#define CANLOAD_RESERVED_BIT_R1 1
-#define CANLOAD_RTR_BITS 1
-#define CANLOAD_IDE_BITS 1
-#define CANLOAD_RESERVED_BIT_R0 1
-#define CANLOAD_DLC_BITS 4
-#define CANLOAD_CRC_BITS 15
-#define CANLOAD_CRC_DELIMITER_BITS 1
-#define CANLOAD_ACK_SLOT_BITS 1
-#define CANLOAD_ACK_DELIMITER_BITS 1
-#define CANLOAD_EOF_BITS 7
-#define CANLOAD_IFS_BITS 3
-
-extern uint32_t can_total_bits;
+#define CANLOAD_BAUD_RATE 500000
 
 float CANLOAD_get_bus_load();
-uint32_t CANLOAD_calculate_message_bits(CAN_msg_t *msg);
+void CANLOAD_update_sliding_window();
+void CANLOAD_calculate_message_bits(uint32_t DLC);
 
 #endif /* INC_CANBUSLOAD_H_ */
