d3f69c021a7692edf2bfe83b0b2921aa79b718b9
Aarjavjain101 <Aarjavjain2736@gmail.com>
2024-11-23 14:36:29 -0800

    Tony uart

--- components/tel/Core/Src/input.py
+++ components/tel/Core/Src/input.py
@@ -1,22 +1,27 @@
 import serial
 import time
 
-# Configure the COM port
-ser = serial.Serial(
-   port='/dev/cu.usbserial-1130',       # COM port (e.g., COM1, COM3, or /dev/ttyUSB0 for Linux)
-   baudrate=115200,     # Baud rate (match with the device's settings)
-)
+# # Configure the COM port
+# ser = serial.Serial(
+#    port='/dev/cu.usbserial-1130',       # COM port (e.g., COM1, COM3, or /dev/ttyUSB0 for Linux)
+#    baudrate=115200,     # Baud rate (match with the device's settings)
+# )
 
-time.sleep(2)
+# time.sleep(2)
 
-try:
-    while True:
-        s = ser.readline()
-        print(s.decode('latin-1'))
+# try:
+#     while True:
+#         s = ser.readline()
+#         print(s.decode('latin-1'))
 
-except KeyboardInterrupt:
-    print("\nProgram interrupted by user. Exiting...")
-except serial.SerialException as e:
-    print(f"Serial error: {e}")
-finally:
-    ser.close()
+# except KeyboardInterrupt:
+#     print("\nProgram interrupted by user. Exiting...")
+# except serial.SerialException as e:
+#     print(f"Serial error: {e}")
+# finally:
+#     ser.close()
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+while True:
+    print(ser.readline().decode('latin-1'))
