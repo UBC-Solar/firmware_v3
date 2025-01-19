ee79a76352b96bec62fb205954076c1a80d12fc3
Makatendeka Chikumbu <91256825+makac1896@users.noreply.github.com>
2025-01-18 20:18:26 -0800

    Firmware updated

--- drd_poc/Core/Src/gpio.c
+++ drd_poc/Core/Src/gpio.c
@@ -32,7 +32,12 @@
 
 /* USER CODE END 1 */
 
-/** Configure pins
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
 */
 void MX_GPIO_Init(void)
 {
