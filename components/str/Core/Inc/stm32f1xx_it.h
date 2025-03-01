922e8517da0dc654a92893eed8cc9e248218c460
Tony Chen <tonychengangyi@gmail.com>
2025-03-01 14:18:19 -0800

    Disabled the interrupts for the RTS_IN and LTS_IN pins

--- components/str/Core/Inc/stm32f1xx_it.h
+++ components/str/Core/Inc/stm32f1xx_it.h
@@ -55,8 +55,6 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-void EXTI0_IRQHandler(void);
-void EXTI1_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
