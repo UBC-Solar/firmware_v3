fe31e2f323003066981aa01fe073a325b0ca8d06
Kyle Groulx <kcgroulx@gmail.com>
2025-03-04 21:07:01 -0800

    over clocked mdi

--- components/mdi/Core/Src/gpio.c
+++ components/mdi/Core/Src/gpio.c
@@ -45,6 +45,7 @@ void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
