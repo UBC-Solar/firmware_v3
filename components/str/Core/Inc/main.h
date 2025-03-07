1fa65a129f9da018710645e83496788a7e7373f8
Tony Chen <81488561+tonychen-2006@users.noreply.github.com>
2025-03-06 18:27:17 -0800

    Feat: STR and Turn Signal Logic (#188)
    * Set up ioc file for STR, no pin figurations yet.
    
    * Added git ignore file by copying and pasting from another file.
    
    * Added the pins for STR ioc
    
    * Added DEBUG LED
    
    * Changed DEBUG label to DEBUG_LED
    
    * Changed CAN baud rate to 500K bit/s on STR ioc file
    
    * Added interrupts to LTS_IN and RTS_IN pins
    
    * Enabled EXT0 and EXT0 pin
    
    * Added CAN for the program, although not sure if externing the variable is working, will look over on Thursday.
    
    * Changed main.c infinite loop
    
    * Fix errors in CAN logic
    
    * Changed interrupt case from rising edge to falling edge.
    
    * Added HAL_CAN_Start and filter.
    
    * Removed unnecessary comments.
    
    * Switched version for firmware_v3.
    
    * Changed to not use interrupts.
    
    * Made changes to CAN implementation so that the values only send when the value for rts or lts changes.
    
    * Disabled the interrupts for the RTS_IN and LTS_IN pins
    
    * removed comments
    
    * Removed additional comments from main.c
    
    * Moved the .settings folder to .gitignore
    
    * Added comments to main function loop.
    
    * Fixed logic with RTS and LTS reading.
    
    * Change code so that RTS and LTS are initialized when the program starts.
    
    * Moved RTS and LTS variables into main.c
    
    * Changed the program to use a enum type.
    
    * Removed CAN filter function.

---
+++ components/str/Core/Inc/main.h
@@ -0,0 +1,79 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LTS_IN_Pin GPIO_PIN_0
+#define LTS_IN_GPIO_Port GPIOA
+#define RTS_IN_Pin GPIO_PIN_1
+#define RTS_IN_GPIO_Port GPIOA
+#define DEBUG_LED_Pin GPIO_PIN_6
+#define DEBUG_LED_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+typedef enum {
+  TS_Off = 0b00,
+  TS_Right = 0b01,
+  TS_Left = 0b10,
+} turn_signal_status_t;
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
