eecac45819ceccd41d3393019b171ac5a1b51ac7
Kyle Groulx <87883655+kcgroulx@users.noreply.github.com>
2025-03-18 20:31:13 -0700

    [FEAT] MDI Refactor (#186)
    * added new mdi project
    
    * updated ioc
    
    * updated ioc
    
    * removed old MDI project
    
    * added basic MDI functionality
    
    * fixed bugs, added dir and eco mode to the motor command
    
    * fixed ioc. Resolved comments
    
    * fixed CAN Rx
    
    * over clocked mdi
    
    * fixed CAN baudrate
    
    * fixed stupid shit

--- components/mdi/Core/Inc/i2c.h
+++ components/mdi/Core/Inc/i2c.h
@@ -1,9 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    can.h
+  * @file    i2c.h
   * @brief   This file contains all the function prototypes for
-  *          the can.c file
+  *          the i2c.c file
   ******************************************************************************
   * @attention
   *
@@ -18,8 +18,8 @@
   */
 /* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __CAN_H__
-#define __CAN_H__
+#ifndef __I2C_H__
+#define __I2C_H__
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,15 +32,13 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern CAN_HandleTypeDef hcan;
+extern I2C_HandleTypeDef hi2c2;
 
 /* USER CODE BEGIN Private defines */
 
-#define DRD_MOTOR_COMMAND_CAN_ID (0x401)
-
 /* USER CODE END Private defines */
 
-void MX_CAN_Init(void);
+void MX_I2C2_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
@@ -50,5 +48,5 @@ void MX_CAN_Init(void);
 }
 #endif
 
-#endif /* __CAN_H__ */
+#endif /* __I2C_H__ */
 
---
+++ components/mdi/Core/Inc/mdi.h
@@ -0,0 +1,51 @@
+/**
+ * @file mdi.h
+ * 
+ * @brief Header file and user facing functions for the MDI
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MDI_H
+#define __MDI_H
+
+// Includes
+#include <stdint.h>
+#include <stdbool.h>
+
+// Macros
+#define MAX_DAC_VALUE (0.90 * 0x3FF)
+#define MAX_TIMEOUT_VALUE (1000) // TODO: Is this time correct?
+#define IS_BIT_SET(num, bit) (((num) & (1 << (bit))) != 0)
+
+
+// Public typedefs
+typedef enum {
+	REGEN_DAC = (0b0001101 << 1),
+    ACCEL_DAC = (0b0001110 << 1)
+} MDI_DAC_addr_t;
+
+typedef enum {
+    FORWARD = 0,
+    REVERSE = 1
+} MDI_direction_t;
+
+typedef enum {
+    ECO_MODE   = 0,
+    POWER_MODE = 1
+} MDI_power_mode_t;
+
+typedef struct{
+    uint16_t accel_DAC_value;
+    uint16_t regen_DAC_value;
+    bool direction_value;
+    bool eco_mode_value;
+} MDI_motor_command_t;
+
+
+// Public functions
+void MDI_set_DAC_voltage(MDI_DAC_addr_t DAC_addr, uint16_t voltage_value);
+void MDI_set_motor_command(MDI_motor_command_t* MDI_motor_command);
+void MDI_parse_motor_command(uint8_t* buffer, MDI_motor_command_t* MDI_motor_command);
+void MDI_stop_motor();
+
+#endif
