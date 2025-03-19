46df1247afd3ef8f645f3c829695e71d84fc0c9a
Evan Owens <evancdowens@gmail.com>
2025-03-18 18:32:55 -0700

    final changes

--- components/drd/Core/Inc/can.h
+++ components/drd/Core/Inc/can.h
@@ -38,25 +38,23 @@ extern CAN_HandleTypeDef hcan;
 extern const CAN_TxHeaderTypeDef drive_command_header;
 extern const CAN_TxHeaderTypeDef mdu_request_header;
 
-#define CAN_ID_STD                  (0x00000000U)  /*!< Standard Id */
-#define CAN_ID_EXT                  (0x00000004U)  /*!< Extended Id */
 
 /*
  *	CAN IDs
  */
 
-#define MOTOR_DRIVE_COMMAND_ADDRESS 0x401
-#define CAN_ID_PACK_CURRENT		    0x450
-#define CAN_ID_BATT_FAULTS		 	0x622
-#define CAN_ID_PACK_VOLTAGE		    0x623
-#define CAN_ID_MTR_FAULTS         	0x08A50225
-#define CAN_ID_MTR_DATA_REQUEST     0x08F89540 //DRD sends data request to the MCU
-#define CAN_ID_TURN_SIGNALS       	0x580 
+#define MOTOR_DRIVE_COMMAND_ADDRESS 	0x401
+#define CAN_ID_PACK_CURRENT		    	0x450
+#define CAN_ID_BATT_FAULTS		 		0x622
+#define CAN_ID_PACK_VOLTAGE		    	0x623
+#define CAN_ID_MTR_FAULTS         		0x08A50225
+#define CAN_ID_MTR_DATA_REQUEST     	0x08F89540 //DRD sends data request to the MCU
+#define CAN_ID_TURN_SIGNALS       		0x580
 
 #define MDU_REQUEST_COMMAND_ID 			0x08F89540
-#define FRAME0 0x08850225
+#define FRAME0 							0x08850225
 #define STR_CAN_MSG_ID					0x580
-#define MDU_REQUEST_FRAME_2_0 5
+#define MDU_REQUEST_FRAME_2_0 			0b101 //request frame 0 and 2 (0th and 2nd bit)
 /* USER CODE END Private defines */
 
 void MX_CAN_Init(void);
