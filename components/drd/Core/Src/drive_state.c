fb7f3c473c600fdd2896666b91fe2341c9a03a20
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-11 16:35:53 -0700

    Logic to hold drive mode value until toggle comes in from 0x580 bit 3 (STR).

--- components/drd/Core/Src/drive_state.c
+++ components/drd/Core/Src/drive_state.c
@@ -495,7 +495,9 @@ void velocity_CAN_msg_handle(uint8_t* data)
  */
 void steering_CAN_msg_handle(uint8_t* data)
 {
-	g_input_flags.eco_mode_on = (data[0] >> 2); //third bit of steering CAN message
+    // If DRIVE MODE bit 3 is 1 then toggle eco_mode_on. Else do nothing
+	g_input_flags.eco_mode_on = (data[0] >> 2) ? !eco_mode_on : eco_mode_on; //third bit of steering CAN message
+    
 	g_lcd_eco_mode_on = g_input_flags.eco_mode_on; //global variable for LCD
 }
 
