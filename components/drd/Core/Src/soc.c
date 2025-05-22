6e98e3cd2c73d5f35691c7c53899971e73898d0c
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-22 00:25:39 -0700

    Another fixup

--- components/drd/Core/Src/soc.c
+++ components/drd/Core/Src/soc.c
@@ -355,7 +355,7 @@ static CAN_TxHeaderTypeDef soc_can_header = {
 #include "CAN_comms.h"
 void SOC_can_tx()
 {
-    uint8_t soc = (uint8_t)SOC_get_soc();
+    uint8_t soc = (uint8_t)(SOC_get_soc() * 100.0f);
 
     CAN_comms_Tx_msg_t time_since_bootup_can_tx = {
         .data[0] = (soc & 0xFF) >> 0,
