1a2ce2e54ae6e7fefdbc62aec59c3c7d5aa2ecdc
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-06-05 01:26:15 -0700

    removed uncenessary

--- components/drd/Core/Src/soc.c
+++ components/drd/Core/Src/soc.c
@@ -1,11 +1,9 @@
 #include "soc.h"
 #include "main.h"
 #include <math.h>
-#include "CAN_comms.h"
 
 volatile float g_total_pack_voltage_soc;
 volatile float g_pack_current_soc;
-
 
 /* DEFINES */
 #define SOC     0
